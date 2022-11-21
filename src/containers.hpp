ad5da6440762d48b4a98b5f7145d7044b9c0959d
sfruano <sergio.fernandezruano@unican.es>
2022-11-21 08:29:56 +0100

    Added function to get geometrical properties of the panel. Changed double precision to cusflaot in order to make the precision optional in the compiling step.

--- src/containers.hpp
+++ src/containers.hpp
@@ -2,12 +2,33 @@
 #ifndef __containers_hpp
 #define __containers_hpp
 
+#include "config.hpp"
+
 struct PanelNodes
 {
-    int num_points=0;
-    double x[4];
-    double y[4];
-    double z[4];
+    int num_points = 0;
+    cusfloat x[4];
+    cusfloat xm = 0;
+    cusfloat y[4];
+    cusfloat ym = 0;
+    cusfloat z[4];
+    cusfloat zm = 0;
+
+    // Add method to calculate the geometric propertiess
+    void calculateGeomProperties(void)
+    {
+        // Calculate ceter of the panel
+        for (int i=0; i<this->num_points; i++)
+        {
+            this->xm += this->x[i];
+            this->ym += this->y[i];
+            this->zm += this->z[i];
+        }
+        this->xm /= this->num_points;
+        this->ym /= this->num_points;
+        this->zm /= this->num_points;
+
+    }
 };
 
 #endif
