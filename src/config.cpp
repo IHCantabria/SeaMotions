06d39ed8efff8867ceae06889cb680225c131ff9
sfruano <sergio.fernandezruano@unican.es>
2022-11-22 15:13:11 +0100

    Added config variables to control the precision in the test performed.

--- src/config.cpp
+++ src/config.cpp
@@ -4,6 +4,8 @@
 
 #ifdef SIMPLE_PREC
 int FLOATING_PRECISION = 32;
+cusfloat EPS_PRECISION = 1e-6;
 #else
 int FLOATING_PRECISION = 64;
+cusfloat EPS_PRECISION = 1e-14;
 #endif
--- src/config.hpp
+++ src/config.hpp
@@ -5,9 +5,11 @@
 #ifdef SIMPLE_PREC
 extern int FLOATING_PRECISION;
 typedef float cusfloat;
+extern cusfloat EPS_PRECISION;
 #else
 extern int FLOATING_PRECISION;
 typedef double cusfloat;
+extern cusfloat EPS_PRECISION;
 #endif
 
 #endif
