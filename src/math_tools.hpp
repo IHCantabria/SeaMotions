804da3bcc21e5fa41c71c98875a4ee8152571fa6
sfruano <sergio.fernandezruano@unican.es>
2022-11-23 12:55:58 +0100

    Deleted vector arithmetic functions. It has been found to be similar ones in Intel MKL library.

--- src/math_tools.hpp
+++ src/math_tools.hpp
@@ -9,11 +9,6 @@ template<typename T> inline int assert_vector_equality(int N, T* u, T* v, T epsi
 template<> inline int assert_vector_equality<int>(int N, int* u, int* v, int epsilon);
 cusfloat* generate_empty_vector(int size);
 template<typename T> void cross(T (&u)[3], T (&v)[3], T (&w)[3]);
-template<typename T> void vAdd(int N, T* u, T* v, T* w);
-template<typename T> void vsAdd(int N, T* u, T s, T* w);
-template<typename T> void vSub(int N, T* u, T* v, T* w);
-template<typename T> void vsSub(int N, T* u, T s, T* w);
-template<typename T> void vsSub(int N, T s, T* u, T* w);
 
 #include "math_tools.txx"
 
