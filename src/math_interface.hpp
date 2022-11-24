b63b39993b8f1e1378bda322d265fd796b73c954
sfruano <sergio.fernandezruano@unican.es>
2022-11-24 17:55:41 +0100

    Added interface for cblas_gemv function (Matrix-Vector multiplication).

--- src/math_interface.hpp
+++ src/math_interface.hpp
@@ -26,6 +26,23 @@ template<>
 inline const auto& lv_add<std::complex<double>> = vzAdd;
 
 
+// Interface for cblas_gemv - Matrix-vector product
+template<typename T>
+inline const auto& cblas_gemv = cblas_sgemv;
+
+template<>
+inline const auto& cblas_gemv<float> = cblas_sgemv;
+
+template<>
+inline const auto& cblas_gemv<double> = cblas_dgemv;
+
+template<>
+inline const auto& cblas_gemv<std::complex<float>> = cblas_cgemv;
+
+template<>
+inline const auto& cblas_gemv<std::complex<double>> = cblas_zgemv;
+
+
 // Interface for cblas_nrm2 - Vector Norm
 template<typename T>
 inline const auto& cblas_nrm2 = cblas_snrm2;
