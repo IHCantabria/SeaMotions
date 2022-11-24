871d2e6f791789cc73d454c2e1e01b753106a530
sfruano <sergio.fernandezruano@unican.es>
2022-11-24 15:53:15 +0100

    Inlined functions in order to ensure that there is only one instance of the function along the code when it is compiled. Removed inclusion of math_tools module.

--- src/math_interface.hpp
+++ src/math_interface.hpp
@@ -8,42 +8,39 @@
 // Include general usage scientific libraries
 #include "mkl.h"
 
-// Include local modules
-#include "math_tools.hpp"
-
 
 // Interface for vAdd - Vector addition
 template<typename T>
-const auto& lv_add = vsAdd;
+inline const auto& lv_add = vsAdd;
 
 template<>
-const auto& lv_add<float> = vsAdd;
+inline const auto& lv_add<float> = vsAdd;
 
 template<>
-const auto& lv_add<double> = vdAdd;
+inline const auto& lv_add<double> = vdAdd;
 
 template<>
-const auto& lv_add<std::complex<float>> = vcAdd;
+inline const auto& lv_add<std::complex<float>> = vcAdd;
 
 template<>
-const auto& lv_add<std::complex<double>> = vzAdd;
+inline const auto& lv_add<std::complex<double>> = vzAdd;
 
 
 // Interface for cblas_nrm2 - Vector Norm
 template<typename T>
-const auto& cblas_nrm2 = cblas_snrm2;
+inline const auto& cblas_nrm2 = cblas_snrm2;
 
 template<>
-const auto& cblas_nrm2<float> = cblas_snrm2;
+inline const auto& cblas_nrm2<float> = cblas_snrm2;
 
 template<>
-const auto& cblas_nrm2<double> = cblas_dnrm2;
+inline const auto& cblas_nrm2<double> = cblas_dnrm2;
 
 template<>
-const auto& cblas_nrm2<std::complex<float>> = cblas_scnrm2;
+inline const auto& cblas_nrm2<std::complex<float>> = cblas_scnrm2;
 
 template<>
-const auto& cblas_nrm2<std::complex<double>> = cblas_dznrm2;
+inline const auto& cblas_nrm2<std::complex<double>> = cblas_dznrm2;
 
 
 #endif
