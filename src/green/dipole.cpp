c894538a2d048f0b875baa49871741ad3605eef0
sfruano <sergio.fernandezruano@unican.es>
2022-11-28 12:32:15 +0100

    Re-structured module in order to have more flexibity to re-use some basic functions.

--- src/green/dipole.cpp
+++ src/green/dipole.cpp
@@ -7,21 +7,32 @@
 #include "mkl.h"
 
 // Include local modules
+#include "common.hpp"
 #include "../containers.hpp"
 #include "dipole.hpp"
 #include "../math_interface.hpp"
 #include "../math_tools.hpp"
 
 
-cusfloat dipole_potential(PanelGeom &panel, cusfloat (&field_point)[3], int fp_local_flag)
+void dipole_potential(PanelGeom &panel, cusfloat (&field_point)[3], int fp_local_flag, cusfloat &phi)
 {
     // Translate field point to panel local coordinates
+    cusfloat field_point_local[3];
     if (fp_local_flag == 0)
     {
         cusfloat field_point_local_aux[3];
-        cusfloat field_point_local[3];
         sv_sub(3, field_point, panel.center, field_point_local_aux);
         cblas_gemv<cusfloat>(CblasRowMajor, CblasNoTrans, 3, 3, 1.0, panel.global_to_local_mat, 3, field_point_local_aux, 1, 0, field_point_local, 1);
+    }
+    else if (fp_local_flag == 1)
+    {
+        field_point_local[0] = field_point[0];
+        field_point_local[1] = field_point[1];
+        field_point_local[2] = field_point[2];
+    }
+    else
+    {
+        throw std::runtime_error("Local flag must have values of: 0 (non-local) and 1 (local)");
     }
 
     // Calculate distances from each node to the field point in local coordinates
@@ -29,39 +40,22 @@ cusfloat dipole_potential(PanelGeom &panel, cusfloat (&field_point)[3], int fp_l
     cusfloat node_fieldp_dy[panel.MAX_PANEL_NODES];
     cusfloat node_fieldp_dz[panel.MAX_PANEL_NODES];
     cusfloat node_fieldp_mod[panel.MAX_PANEL_NODES];
-    cusfloat node_fieldp_vec[3];
-    cusfloat node_pos[3];
-    for (int i=0; i<panel.num_nodes; i++)
-    {
-        panel.get_node_local_position(i, node_pos);
-        sv_sub(3, field_point_local, node_pos, node_fieldp_vec);
-        sv_mod(3, node_fieldp_vec, node_fieldp_mod[i]);
-
-        // Storage vector components for futher use
-        node_fieldp_dx[i] = node_fieldp_vec[0];
-        node_fieldp_dy[i] = node_fieldp_vec[1];
-        node_fieldp_dz[i] = node_fieldp_vec[2];
-    }
+    calculate_distance_node_field(panel, field_point_local, node_fieldp_mod, node_fieldp_dx, node_fieldp_dy, node_fieldp_dz);
 
     // Calculate distances in between nodes
     cusfloat delta_xi [panel.MAX_PANEL_NODES];
     cusfloat delta_eta [panel.MAX_PANEL_NODES];
+    calculate_nodes_distance(panel, delta_xi, delta_eta);
 
-    for (int i=0; i<panel.num_nodes-1; i++)
-    {
-        delta_xi[i] = panel.xl[i+1] - panel.xl[i];
-        delta_eta[i] = panel.yl[i+1] - panel.yl[i];
-    }
-    delta_xi[panel.num_nodes-1] = panel.xl[0]-panel.xl[panel.num_nodes-1];
-    delta_eta[panel.num_nodes-1] = panel.yl[0]-panel.yl[panel.num_nodes-1];
+    // Calculate potential kernel
+    calculate_dipole_potential_kernel(panel, node_fieldp_mod, node_fieldp_dx, node_fieldp_dy, node_fieldp_dz, delta_xi, delta_eta, phi);
+   
+}
 
-    std::cout << "XL: "; print_vector(panel.num_nodes, panel.xl, 0);
-    std::cout << "YL: "; print_vector(panel.num_nodes, panel.yl, 0);
-    std::cout << "dXi: "; print_vector(panel.num_nodes, delta_xi, 0);
-    std::cout << "dEta: "; print_vector(panel.num_nodes, delta_eta, 0);
 
-    // Calculate potential
-    cusfloat phi = 0.0;
+void calculate_dipole_potential_kernel(PanelGeom &panel, cusfloat * node_fieldp_mod, cusfloat* node_fieldp_dx, 
+    cusfloat* node_fieldp_dy, cusfloat* node_fieldp_dz, cusfloat* delta_xi, cusfloat* delta_eta, cusfloat &phi)
+{
     cusfloat a0, a1, a2;
     cusfloat b0, b1, b2;
     for (int i=0; i<panel.num_nodes-1; i++)
@@ -81,6 +75,4 @@ cusfloat dipole_potential(PanelGeom &panel, cusfloat (&field_point)[3], int fp_l
     b1 = delta_xi[n]*node_fieldp_dx[0]*node_fieldp_dy[0];
     b2 = node_fieldp_mod[0]*node_fieldp_dz[0]*delta_xi[n];
     phi += atan((a0-a1)/a2) - atan((b0-b1)/b2);
-
-    return phi;
 }
--- src/green/dipole.hpp
+++ src/green/dipole.hpp
@@ -5,6 +5,8 @@
 #include "../config.hpp"
 #include "../containers.hpp"
 
-cusfloat dipole_potential(PanelGeom &panel, cusfloat (&field_point)[3], int fp_local_flag);
+void dipole_potential(PanelGeom &panel, cusfloat (&field_point)[3], int fp_local_flag, cusfloat &phi);
+void calculate_dipole_potential_kernel(PanelGeom &panel, cusfloat * node_fieldp_mod, cusfloat* node_fieldp_dx, 
+    cusfloat* node_fieldp_dy, cusfloat* node_fieldp_dz, cusfloat* delta_xi, cusfloat* delta_eta, cusfloat &phi);
 
 #endif
