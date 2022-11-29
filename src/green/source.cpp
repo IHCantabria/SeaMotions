cab637f30ba5886c17d8360a219f9ab0e626f43f
sfruano <sergio.fernandezruano@unican.es>
2022-11-29 12:12:48 +0100

    Added velocity calcutaion using Hess and Smith formulas.

--- src/green/source.cpp
+++ src/green/source.cpp
@@ -152,3 +152,70 @@ void calculate_source_velocity(PanelGeom &panel, cusfloat (&field_point)[3], int
     }
 
 }
+
+
+void calculate_source_velocity_hess(PanelGeom &panel, cusfloat (&field_point)[3], int fp_local_flag, cusfloat (&velocity)[3])
+{
+    // Translate field point to panel local coordinates
+    cusfloat field_point_local[3];
+    if (fp_local_flag == 0)
+    {
+        cusfloat field_point_local_aux[3];
+        sv_sub(3, field_point, panel.center, field_point_local_aux);
+        cblas_gemv<cusfloat>(CblasRowMajor, CblasNoTrans, 3, 3, 1.0, panel.global_to_local_mat, 3, field_point_local_aux, 1, 0, field_point_local, 1);
+    }
+    else if (fp_local_flag == 1)
+    {
+        field_point_local[0] = field_point[0];
+        field_point_local[1] = field_point[1];
+        field_point_local[2] = field_point[2];
+    }
+    else
+    {
+        throw std::runtime_error("Local flag must have values of: 0 (non-local) and 1 (local)");
+    }
+
+    // Calculate distances from each node to the field point in local coordinates
+    cusfloat node_fieldp_dx[panel.MAX_PANEL_NODES];
+    cusfloat node_fieldp_dy[panel.MAX_PANEL_NODES];
+    cusfloat node_fieldp_dz[panel.MAX_PANEL_NODES];
+    cusfloat node_fieldp_mod[panel.MAX_PANEL_NODES];
+    calculate_distance_node_field(panel, field_point_local, node_fieldp_mod, node_fieldp_dx, node_fieldp_dy, node_fieldp_dz);
+
+    // Calculate distances in between nodes
+    cusfloat delta_xi [panel.MAX_PANEL_NODES];
+    cusfloat delta_eta [panel.MAX_PANEL_NODES];
+    calculate_nodes_distance(panel, delta_xi, delta_eta);
+
+    cusfloat sides_len [panel.MAX_PANEL_NODES];
+    calculate_sides_len_local(panel, delta_xi, delta_eta, sides_len);
+
+    // Calculate velocities
+    cusfloat eki, eki1, hki, hki1, mi, r_sum, log_r;
+    int i1 = 0;
+    for (int i=0; i<panel.num_nodes; i++)
+    {
+        // Calculate forward index
+        i1 = (i+1)%panel.num_nodes;
+
+        // Calculate local quatities
+        eki = pow2s(node_fieldp_dz[i]) + pow2s(node_fieldp_dx[i]);
+        eki1 = pow2s(node_fieldp_dz[i1]) + pow2s(node_fieldp_dx[i1]);
+        hki = node_fieldp_dy[i]*node_fieldp_dx[i];
+        hki1 = node_fieldp_dy[i1]*node_fieldp_dx[i1];
+        r_sum = node_fieldp_mod[i]+node_fieldp_mod[i1];
+        log_r = std::log((r_sum-sides_len[i])/(r_sum+sides_len[i]));
+
+        // Calculate X velocity
+        velocity[0] += delta_eta[i]/sides_len[i]*log_r;
+
+        // Calculate Y velocity
+        velocity[1] -= delta_xi[i]/sides_len[i]*log_r;
+
+        // Calculate Z velocity
+        mi = delta_eta[i]/delta_xi[i];
+        velocity[2] += std::atan((mi*eki-hki)/(node_fieldp_dz[i]*node_fieldp_mod[i]));
+        velocity[2] -= std::atan((mi*eki1-hki1)/(node_fieldp_dz[i1]*node_fieldp_mod[i1]));
+        
+    }
+}
