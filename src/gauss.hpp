8178cbad96ac7692abf16ad5755550b4fb51c520
sfruano <sergio.fernandezruano@unican.es>
2022-12-05 14:08:12 +0100

    Added new module gauss which will contain the gauss points for numerical integration of functions.

---
+++ src/gauss.hpp
@@ -0,0 +1,11 @@
+
+#ifndef __gauss_hpp
+#define __gauss_hpp
+
+// Include local modules
+#include "config.hpp"
+
+void get_gauss_legendre(int num_points, cusfloat* weights, cusfloat* points);
+
+
+#endif
