062d666602bbddd293e7cd53569c6d8e86977d5a
Sergio DesktopPC <feruanos@unican.es>
2022-12-08 15:37:48 +0100

    Added struve functions of the first and second order. Furthermore, it has been added some auxiliary functions to calculate them.

--- src/special_math.cpp
+++ src/special_math.cpp
@@ -66,6 +66,56 @@ cusfloat rational_fraction_f1(cusfloat x)
 }
 
 
+cusfloat rational_fraction_struve0(cusfloat x)
+{
+    // Define local constans
+    cusfloat a0 = 0.99999906;
+    cusfloat a1 = 4.77228920;
+    cusfloat a2 = 3.85542044;
+    cusfloat a3 = 0.32303607;
+
+    cusfloat b1 = 4.88331068;
+    cusfloat b2 = 4.28957333;
+    cusfloat b3 = 0.52120508;
+
+    // Compute struve factor
+    cusfloat xi = 3.0/x;
+    cusfloat x2 = xi*xi;
+    cusfloat x4 = x2*x2;
+    cusfloat x6 = x2*x4;
+    cusfloat c0 = 2*(a0+a1*x2+a2*x4+a3*x6);
+    cusfloat c1 = PI*x*(1+b1*x2+b2*x4+b3*x6);
+    cusfloat sf = c0/c1;
+
+    return sf;
+}
+
+
+cusfloat rational_fraction_struve1(cusfloat x)
+{
+    // Define local constans
+    cusfloat a0 = 1.00000004;
+    cusfloat a1 = 3.92205313;
+    cusfloat a2 = 2.64893033;
+    cusfloat a3 = 0.27450895;
+
+    cusfloat b1 = 3.81095112;
+    cusfloat b2 = 2.26216956;
+    cusfloat b3 = 0.10885141;
+
+    // Compute struve factor
+    cusfloat xi = 3.0/x;
+    cusfloat x2 = xi*xi;
+    cusfloat x4 = x2*x2;
+    cusfloat x6 = x2*x4;
+    cusfloat c0 = 2*(a0+a1*x2+a2*x4+a3*x6);
+    cusfloat c1 = PI*(1+b1*x2+b2*x4+b3*x6);
+    cusfloat sf = c0/c1;
+
+    return sf;
+}
+
+
 cusfloat rational_fraction_th0(cusfloat x)
 {
     // Define local constants
@@ -288,6 +338,100 @@ cusfloat bessely1(cusfloat x)
     else
     {
         sol = 1/std::sqrt(x)*rational_fraction_f1(x)*std::sin(rational_fraction_th1(x));
+    }
+
+    return sol;
+}
+
+
+cusfloat struve0(cusfloat x)
+{
+    // Check if the input is a real positive number
+    if (x < 0.0)
+    {
+        std::string err_message("Struve function of first order not defined for x < 0.0.");
+        std::cerr << err_message << std::endl;
+        throw std::runtime_error(err_message);
+    }
+
+    // Declare local variables
+    cusfloat sol = 0.0;
+
+    // Calculate polynomial approximation for 0 <= x <= 3
+    if (x <= 3.0)
+    {
+        // Calculate polynomial coordinate to avoid
+        // repeated calculation of it
+        cusfloat xi = x/3.0;
+
+        // Calculate power coefficients to economize
+        // the computational effort
+        cusfloat p2 = std::pow(xi, 2.0);
+        cusfloat pc = xi;
+
+        // Calculate polynomial approximation
+        sol += 1.909859164*pc;
+        pc *= p2;
+        sol -= 1.909855001*pc;
+        pc *= p2;
+        sol += 0.687514637*pc;
+        pc *= p2;
+        sol -= 0.126164557*pc;
+        pc *= p2;
+        sol += 0.013828813*pc;
+        pc *= p2;
+        sol -= 0.000876918*pc;
+    }
+    else
+    {
+        sol = bessely0(x) + rational_fraction_struve0(x);
+    }
+
+    return sol;
+}
+
+
+cusfloat struve1(cusfloat x)
+{
+    // Check if the input is a real positive number
+    if (x < 0.0)
+    {
+        std::string err_message("Struve function of second order not defined for x < 0.0.");
+        std::cerr << err_message << std::endl;
+        throw std::runtime_error(err_message);
+    }
+
+    // Declare local variables
+    cusfloat sol = 0.0;
+
+    // Calculate polynomial approximation for 0 <= x <= 3
+    if (x <= 3.0)
+    {
+        // Calculate polynomial coordinate to avoid
+        // repeated calculation of it
+        cusfloat xi = x/3.0;
+
+        // Calculate power coefficients to economize
+        // the computational effort
+        cusfloat p2 = std::pow(xi, 2.0);
+        cusfloat pc = p2;
+
+        // Calculate polynomial approximation
+        sol += 1.909859286*pc;
+        pc *= p2;
+        sol -= 1.145914713*pc;
+        pc *= p2;
+        sol += 0.294656958*pc;
+        pc *= p2;
+        sol -= 0.042070508*pc;
+        pc *= p2;
+        sol += 0.003785727*pc;
+        pc *= p2;
+        sol -= 0.000207183*pc;
+    }
+    else
+    {
+        sol = bessely1(x) + rational_fraction_struve1(x);
     }
 
     return sol;
