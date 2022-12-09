f376a790525486b1a6161c0dfd0ece2fab594acb
Sergio DesktopPC <feruanos@unican.es>
2022-12-09 09:41:23 +0100

    Declare bessel funciton in the header file of the module

--- src/special_math.hpp
+++ src/special_math.hpp
@@ -5,4 +5,14 @@
 // Include local modules
 #include "config.hpp"
 
+
+cusfloat polynomial_f0(cusfloat x);
+cusfloat polynomial_f1(cusfloat x);
+cusfloat polynomial_th0(cusfloat x);
+cusfloat polynomial_th1(cusfloat x);
+cusfloat rational_fraction_f0(cusfloat x);
+cusfloat rational_fraction_f1(cusfloat x);
+cusfloat rational_fraction_th0(cusfloat x);
+cusfloat rational_fraction_th1(cusfloat x);
+
 #endif
