ec333218575bee9512ed2c7b17234762916b1cfd
sfruano <sergio.fernandezruano@unican.es>
2022-12-15 15:09:44 +0100

    Added test for legendre polynomial.

--- tests/tools/test_legendre_poly.cpp
+++ tests/tools/test_legendre_poly.cpp
@@ -1,6 +1,7 @@
 
 // Include general usage libraries
 #include <functional>
+#include <iomanip>
 #include <iostream>
 
 // Include local modules
@@ -9,13 +10,15 @@
 #include "../../src/special_math.hpp"
 #include "../../src/tools.hpp"
 
+#ifdef SIMPLE_PREC
+cusfloat EPS_LEGENDRE = 1e-6;
+#else
+cusfloat EPS_LEGENDRE = 1e-12;
+#endif
 
-bool launch_test(std::string file_path, int order)
+
+bool launch_test(DataRef &data_ref, int order)
 {
-    // Load reference data
-    DataRef data_ref;
-    data_ref.read_data(file_path);
-
     // Loop over reference data to check the target
     // function
     cusfloat fi = 0;
@@ -24,11 +27,12 @@ bool launch_test(std::string file_path, int order)
     for (int i=0; i<data_ref.num_points; i++)
     {
         fi = legendre_poly_raw(order, data_ref.x[i]);
-        diff = data_ref.y[i] - fi;
-        if (std::abs(fi-diff)>EPS_PRECISION)
+        diff = data_ref.data[i][order] - fi;
+        if (std::abs(diff)>EPS_LEGENDRE)
         {
-            std::cerr << "X: " << data_ref.x[i] << " - Yf: " << fi;
-            std::cerr << " - Yref:" << data_ref.y[i] << std::endl;
+            std::cerr << std::setprecision(15) << "X: " << data_ref.x[i] << " - Yf: " << fi;
+            std::cerr << std::setprecision(15) << " - Yref:" << data_ref.data[i][order];
+            std::cerr << " - Diff: " << diff << std::endl;
             pass = false;
             break;
         }
@@ -41,112 +45,30 @@ bool launch_test(std::string file_path, int order)
 int main(int argc, char* argv[])
 {
     // Read command line arguments
-    if (!check_num_cmd_args(argc, 11))
+    if (!check_num_cmd_args(argc, 1))
     {
         return 1;
     }
 
-    std::string file_path_legendre0(argv[1]);
-    std::string file_path_legendre1(argv[2]);
-    std::string file_path_legendre2(argv[3]);
-    std::string file_path_legendre3(argv[4]);
-    std::string file_path_legendre4(argv[5]);
-    std::string file_path_legendre5(argv[6]);
-    std::string file_path_legendre6(argv[7]);
-    std::string file_path_legendre7(argv[8]);
-    std::string file_path_legendre8(argv[9]);
-    std::string file_path_legendre9(argv[10]);
-    std::string file_path_legendre10(argv[11]);
+    std::string file_path_legendre(argv[1]);
 
     // Define local variables
     bool pass = false;
 
+    // Load reference data
+    DataRef data_ref;
+    data_ref.read_multiple_channel(file_path_legendre);
+
     // Launch test for zeroth order Legendre polynomial
-    pass = launch_test(file_path_legendre0, 0);
-    if (!pass)
+    for (int i=0; i<data_ref.num_channels; i++)
     {
-        std::cerr << "test_legendre_poly/legendre_poly Order 0 failed!" << std::endl;
-        return 1;
-    }
+        pass = launch_test(data_ref, i);
+        if (!pass)
+        {
+            std::cerr << "test_legendre_poly/legendre_poly Order 0 failed!" << std::endl;
+            return 1;
+        }
 
-    // Launch test for first order Legendre polynomial
-    pass = launch_test(file_path_legendre1, 1);
-    if (!pass)
-    {
-        std::cerr << "test_legendre_poly/legendre_poly Order 1 failed!" << std::endl;
-        return 1;
-    }
-
-    // Launch test for second order Legendre polynomial
-    pass = launch_test(file_path_legendre2, 2);
-    if (!pass)
-    {
-        std::cerr << "test_legendre_poly/legendre_poly Order 2 failed!" << std::endl;
-        return 1;
-    }
-
-    // Launch test for third order Legendre polynomial
-    pass = launch_test(file_path_legendre3, 3);
-    if (!pass)
-    {
-        std::cerr << "test_legendre_poly/legendre_poly Order 3 failed!" << std::endl;
-        return 1;
-    }
-
-    // Launch test for fourth order Legendre polynomial
-    pass = launch_test(file_path_legendre4, 4);
-    if (!pass)
-    {
-        std::cerr << "test_legendre_poly/legendre_poly Order 4 failed!" << std::endl;
-        return 1;
-    }
-
-    // Launch test for fifth order Legendre polynomial
-    pass = launch_test(file_path_legendre5, 5);
-    if (!pass)
-    {
-        std::cerr << "test_legendre_poly/legendre_poly Order 4 failed!" << std::endl;
-        return 1;
-    }
-
-    // Launch test for sixth order Legendre polynomial
-    pass = launch_test(file_path_legendre6, 6);
-    if (!pass)
-    {
-        std::cerr << "test_legendre_poly/legendre_poly Order 6 failed!" << std::endl;
-        return 1;
-    }
-
-    // Launch test for seventh order Legendre polynomial
-    pass = launch_test(file_path_legendre7, 7);
-    if (!pass)
-    {
-        std::cerr << "test_legendre_poly/legendre_poly Order 7 failed!" << std::endl;
-        return 1;
-    }
-
-    // Launch test for eight order Legendre polynomial
-    pass = launch_test(file_path_legendre8, 8);
-    if (!pass)
-    {
-        std::cerr << "test_legendre_poly/legendre_poly Order 8 failed!" << std::endl;
-        return 1;
-    }
-
-    // Launch test for ninth order Legendre polynomial
-    pass = launch_test(file_path_legendre9, 9);
-    if (!pass)
-    {
-        std::cerr << "test_legendre_poly/legendre_poly Order 9 failed!" << std::endl;
-        return 1;
-    }
-
-    // Launch test for tenth order Legendre polynomial
-    pass = launch_test(file_path_legendre10, 10);
-    if (!pass)
-    {
-        std::cerr << "test_legendre_poly/legendre_poly Order 10 failed!" << std::endl;
-        return 1;
     }
 
     return 0;
