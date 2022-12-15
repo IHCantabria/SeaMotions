d146e3497cd17e33151cbbdeb444bc43a556b908
sfruano <sergio.fernandezruano@unican.es>
2022-12-15 14:14:30 +0100

    Added method to read input files with multiple channels.

--- tests/containers.hpp
+++ tests/containers.hpp
@@ -11,14 +11,72 @@
 
 struct DataRef
 {
+    int num_channels = 0;
     int num_points = 0;
     cusfloat* x = nullptr;
     cusfloat* y = nullptr;
+    cusfloat** data = nullptr;
 
     ~DataRef()
     {
+        // Deallocate memory
         delete [] this->x;
         delete [] this->y;
+
+        if (this->num_channels > 0)
+        {
+            for (int i=0; i<this->num_channels; i++)
+            {
+                delete [] this->data[i];
+            }
+            delete [] this->data;
+        }
+    }
+
+    void read_multiple_channel(std::string file_path)
+    {
+        // Declare local variables
+        std::string s0, s1, s2;
+
+        // Open file unit
+        std::ifstream infile(file_path);
+
+        // Read number of points
+        infile >> s0 >> s1 >> s2;
+        infile >> this->num_points;
+
+        // Read number of channels
+        infile >> s0 >> s1 >> s2;
+        infile >> this->num_channels;
+
+        // Allocate heap memory for the data
+        this->x = new cusfloat [this->num_points];
+        this->data = new cusfloat* [this->num_points];
+        for (int i=0; i<this->num_points; i++)
+        {
+            this->data[i] = new cusfloat [num_channels];
+        }
+
+        // Read data header from file
+        infile >> s0;
+        for (int i=0; i<this->num_channels; i++)
+        {
+            infile >> s1;
+        }
+
+        // Read data from file
+        for (int i=0; i<this->num_points; i++)
+        {
+            infile >> this->x[i];
+            for (int j=0; j<this->num_channels; j++)
+            {
+                infile >> this->data[i][j];
+            }
+        }
+
+        // Close file unit
+        infile.close();
+
     }
 
     void read_single_channel(std::string file_path)
