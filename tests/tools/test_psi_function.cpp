123f0fe4157ed4e0ff9749126560bffdec606f78
sfruano <sergio.fernandezruano@unican.es>
2022-12-15 12:32:02 +0100

    Changed psi function test to use generic DataRef struct in tests/containers module.

--- tests/tools/test_psi_function.cpp
+++ tests/tools/test_psi_function.cpp
@@ -6,52 +6,10 @@
 #include <string>
 
 // Include local modules
+#include "../containers.hpp"
 #include "../../src/config.hpp"
 #include "../../src/special_math.hpp"
 #include "../../src/tools.hpp"
-
-
-struct DataRef
-{
-    int num_points = 0;
-    cusfloat* x = nullptr;
-    cusfloat* y = nullptr;
-
-    ~DataRef()
-    {
-        delete [] this->x;
-        delete [] this->y;
-    }
-
-    void read_data(std::string file_path)
-    {
-        // Declare local variables
-        std::string s0, s1, s2;
-
-        // Open file unit
-        std::ifstream infile(file_path);
-
-        // Read number of points
-        infile >> s0 >> s1 >> s2;
-        infile >> this->num_points;
-
-        // Allocate heap memory for the data
-        this->x = new cusfloat [this->num_points];
-        this->y = new cusfloat [this->num_points];
-
-        // Read data from file
-        infile >> s0 >> s1;
-        for (int i=0; i<this->num_points; i++)
-        {
-            infile >> this->x[i] >> this->y[i];
-        }
-
-        // Close file unit
-        infile.close();
-
-    }
-
-};
 
 
 int main(int argc, char* argv[])
