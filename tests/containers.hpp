5047d8547cac7d7eb520ed70cf5c0fdc531c1503
sfruano <sergio.fernandezruano@unican.es>
2022-12-15 12:26:12 +0100

    Added container class to load data from file and store two channels.

---
+++ tests/containers.hpp
@@ -0,0 +1,54 @@
+
+#ifndef __test_containers_hpp
+#define __test_containers_hpp
+
+// Include general usage libraries
+#include <fstream>
+#include <string>
+
+// Include local modules
+#include "../src/config.hpp"
+
+struct DataRef
+{
+    int num_points = 0;
+    cusfloat* x = nullptr;
+    cusfloat* y = nullptr;
+
+    ~DataRef()
+    {
+        delete [] this->x;
+        delete [] this->y;
+    }
+
+    void read_data(std::string file_path)
+    {
+        // Declare local variables
+        std::string s0, s1, s2;
+
+        // Open file unit
+        std::ifstream infile(file_path);
+
+        // Read number of points
+        infile >> s0 >> s1 >> s2;
+        infile >> this->num_points;
+
+        // Allocate heap memory for the data
+        this->x = new cusfloat [this->num_points];
+        this->y = new cusfloat [this->num_points];
+
+        // Read data from file
+        infile >> s0 >> s1;
+        for (int i=0; i<this->num_points; i++)
+        {
+            infile >> this->x[i] >> this->y[i];
+        }
+
+        // Close file unit
+        infile.close();
+
+    }
+
+};
+
+#endif
