bcb3f70e773b2487443b691c8063f28c0291a20b
sfruano <sergio.fernandezruano@unican.es>
2022-12-22 16:14:13 +0100

    Add test to check the infinite water depth Newman series expansion.

--- tests/green/test_wave_term_inf_depth.cpp
+++ tests/green/test_wave_term_inf_depth.cpp
@@ -10,23 +10,71 @@
 #include "../../src/special_math.hpp"
 
 
+void generate_domain(int N, cusfloat* X, cusfloat* Y, cusfloat x_min, cusfloat x_max,
+    cusfloat y_min, cusfloat y_max)
+{
+    // Generate X coordinate points
+    cusfloat dx = (x_max-x_min)/(N-1);
+    for (int i=0; i<N; i++)
+    {
+        X[i] = x_min + i*dx;
+    }
+
+    // Generate Y coordinate points
+    cusfloat dy = (y_max-y_min)/(N-1);
+    for (int i=0; i<N; i++)
+    {
+        Y[i] = y_min + i*dy;
+    }
+}
+
+
+bool launch_test(void)
+{
+    // Generate computational grid
+    constexpr int N = 1000;
+    cusfloat X[N], Y[N];
+    generate_domain(N, X, Y, 0.001, 40.0, 0.001, 40.0);
+
+    // Loop over grid points to check the accuracy of the model
+    bool pass = true;
+    cusfloat diff = 0.0;
+    cusfloat f_ref = 0.0, f_mod = 0.0;
+    for (int i=0; i<N; i++)
+    {
+        for (int j=0; j<N; j++)
+        {
+            // Calculate reference value using Romberg integration method
+            f_ref = wave_term_inf_depth_num(X[i], Y[j]) + 1/sqrt(pow2s(X[i])+pow2s(Y[j]));
+
+            // Calculate model value
+            f_mod = wave_term_inf_depth(X[i], Y[j]);
+
+            // Evaluate difference
+            diff = f_mod - f_ref;
+            if (std::abs(diff) > 3e-6)
+            {
+                std::cerr << "X: " << X[i] << " - Y: " << Y[j] << " - f_ref: " << f_ref;
+                std::cerr << " - f_mod: " << f_mod << " - diff: " << diff << std::endl;
+                pass = false;
+                break;
+            }
+        }
+    }
+
+    return pass;
+}
+
+
 int main(void)
 {
-    cusfloat X = 10.0;
-    cusfloat Y = 25.0;
-
-    cusfloat f0 = wave_term_inf_depth_num(X, Y) + 1/sqrt(pow2s(X)+pow2s(Y));
-    // cusfloat f0 = romberg_quadrature(
-    //     [X, Y](cusfloat t)->cusfloat {return expint_inf_depth_num(X, Y, t);},
-    //     0.0,
-    //     Y,
-    //     1e-12
-    // );
-    cusfloat f1 = wave_term_inf_depth(X, Y);
-
-    std::cout << std::setprecision(15) << "f0: " << f0 << " - f1: " << f1;
-    std::cout << " - Diff: " << std::abs(f1-f0) << std::endl;
-
+    // Test modelling function over all XY plane
+    int pass = launch_test();
+    if (!pass)
+    {
+        std::cerr << "test_wave_term_inf_depth failed!" << std::endl;
+        return 1;
+    }
 
     return 0;
 }
