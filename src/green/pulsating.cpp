77d3694c2ca351deba60e10415bc1070247f0b6e
sfruano <sergio.fernandezruano@unican.es>
2022-12-23 08:42:56 +0100

    Added function to calculate the derivative of the infinite wave term series using the Newman method.

--- src/green/pulsating.cpp
+++ src/green/pulsating.cpp
@@ -145,7 +145,7 @@ void get_inf_domain_bounds(cusfloat x, cusfloat y, cusfloat &x0, cusfloat &x1, c
 cusfloat wave_term_inf_depth(cusfloat X, cusfloat Y)
 {
     cusfloat wave_term = 0.0;
-    if ((X>8.0) && (Y>=20.0))
+    if ((X>=8.0) && (Y>=20.0))
     {
         // Calculate formulation parameters
         cusfloat R = std::sqrt(pow2s(X)+pow2s(Y));
@@ -164,6 +164,268 @@ cusfloat wave_term_inf_depth(cusfloat X, cusfloat Y)
         }
 
         wave_term = rfs - PI*std::exp(-Y)*(struve0(X)+bessely0(X))-2.0*wave_term;
+    }
+    else if (Y > 2*X)
+    {
+        // Add radius term
+        wave_term = 1/std::sqrt(pow2s(X)+pow2s(Y));
+
+        // Define local variables
+        cusfloat cumsum_n = 0.0;
+        cusfloat cumsum_m = 0.0;
+        cusfloat fx = -pow2s(X)/4.0;
+        cusfloat fy = 1/Y;
+        cusfloat tx = fx;
+        cusfloat ty = fy*fy;
+        cusfloat fn = 2.0;
+        cusfloat fm = 2.0;
+        cusfloat c_expi = std::exp(-Y)*expint_i(Y);
+
+        // Add n=0 and n=1 terms
+        cumsum_m = (fy+ty);
+        cumsum_n = -c_expi + tx*(cumsum_m-c_expi);
+        tx *= fx;
+        ty *= fy;
+
+        // Add terms up to n=9
+        for (int n=2; n<=9; n++)
+        {
+            // Calculate m loop value
+            for (int m=2*(n-1)+1; m<=2*n; m++)
+            {
+                // Upate m loop
+                cumsum_m += fm*ty;
+
+                // Calculate m loop variables
+                fm *= m;
+                ty *= fy;
+            }
+
+            // Calculate n loop contribution
+            cumsum_n += tx/pow2s(fn)*(cumsum_m-c_expi);
+
+            // Update n loop variables
+            tx *= fx;
+            fn *= n+1;
+        }
+        wave_term += 2.0*cumsum_n;
+    }
+    else if ((X >= 3.7) && (Y <= 0.25*X))
+    {
+        cusfloat fs = 1.0;
+        cusfloat fn = 1.0;
+        cusfloat fx = 1/pow2s(X);
+        cusfloat fy = pow2s(Y);
+        cusfloat tx = 1.0;
+        cusfloat ty = 1.0;
+        cusfloat fmult = 1.0;
+        cusfloat i2n = 1-std::exp(-Y);
+        for (int i=0; i<=3; i++)
+        {
+            // Add expansion series term
+            wave_term += fs*tx/fn*fmult*i2n;
+
+            // Update loop variables
+            fs *= -1.0;
+            fn *= (i+1);
+            tx *= fx;
+            ty *= fy;
+            fmult *= (2*(i+1)-1.0)/2.0;
+            i2n = ty - 2.0*(i+1.0)*ty/Y + 2.0*(i+1.0)*(2.0*(i+1.0)-1.0)*i2n;
+        }
+        wave_term /= X;
+        wave_term = 1/std::sqrt(pow2s(X)+pow2s(Y)) - PI*std::exp(-Y)*(struve0(X)+bessely0(X)) - 2.0*wave_term;
+    }
+    else if ((X>0)&&(X<=3.7)&&(Y>0.0)&&(Y<=2.0))
+    {
+        // Define radial distance to be used along the module
+        cusfloat R = std::sqrt(pow2s(X)+pow2s(Y));
+
+        // Define factor variables
+        cusfloat fx = pow2s(X);
+        cusfloat tx = fx;
+        cusfloat ty = 0.0;
+
+        // Calculate double series first row of coefficients
+        constexpr int N = 9;
+        cusfloat cmn_coeffs[2*N+1];
+        cusfloat fn = 2.0;
+        cusfloat cumsum_cmn = 0.0;
+        ty = Y;
+        for (int n=1; n<=(2*N+1); n++)
+        {
+            cmn_coeffs[n-1] = 1.0/fn/(n+1);
+            cumsum_cmn += cmn_coeffs[n-1]*ty;
+            fn *= (n+2);
+            ty *= Y;
+        }
+
+        // Calculate double series of coefficients
+        cusfloat nf = 0.0;
+        for (int m=1; m<N; m++)
+        {
+            ty = Y;
+            for (int n=1; n<=(2*N+1-2*m); n++)
+            {
+                // Update n as float in order to perform the math operations
+                // correctly
+                nf = static_cast<cusfloat>(n);
+
+                // Update series coefficients
+                cmn_coeffs[n-1] = -((nf+2.0)/(nf+1.0))*cmn_coeffs[n+1];
+                
+                // Add new term to the series
+                cumsum_cmn += cmn_coeffs[n-1]*tx*ty;
+
+                ty *= Y;
+            }
+            // Update series coefficients
+            tx *= fx;
+        }
+        cumsum_cmn += cmn_coeffs[0]*tx*Y;
+        cumsum_cmn *= R;
+
+        // Addd rest of the function
+        wave_term = 1/R - 2.0*std::exp(-Y)*(besselj0(X)*std::log(Y/X+std::sqrt(1.0+pow2s(Y/X)))
+                                            + PI*bessely0(X)/2.0
+                                            + PI*struve0(X)*R/2.0/X
+                                            + cumsum_cmn
+                                            );
+    }
+    else if ((X>=3.7)&&(Y<=2.0))
+    {
+        cusfloat fs = 1.0;
+        cusfloat fn = 1.0;
+        cusfloat fx = 1/pow2s(X);
+        cusfloat fy = pow2s(Y);
+        cusfloat tx = 1.0;
+        cusfloat ty = 1.0;
+        cusfloat fmult = 1.0;
+        cusfloat i2n = 1-std::exp(-Y);
+        for (int i=0; i<=7; i++)
+        {
+            // Add expansion series term
+            wave_term += fs*tx/fn*fmult*i2n;
+
+            // Update loop variables
+            fs *= -1.0;
+            fn *= (i+1);
+            tx *= fx;
+            ty *= fy;
+            fmult *= (2*(i+1)-1.0)/2.0;
+            i2n = ty - 2.0*(i+1.0)*ty/Y + 2.0*(i+1.0)*(2.0*(i+1.0)-1.0)*i2n;
+        }
+        wave_term /= X;
+        wave_term = 1/std::sqrt(pow2s(X)+pow2s(Y)) - PI*std::exp(-Y)*(struve0(X)+bessely0(X)) - 2.0*wave_term;
+    }
+    else
+    {
+        // Calculate expint residual values - R(x,y)
+        cusfloat rxy = 0.0;
+        if (Y<=4.0)
+        {
+            if (X <= 3.0)
+            {
+                rxy = eval_chebyshev_fit(
+                    chebyinf::num_cheby_a0,
+                    chebyinf::cheby_coeff_a0,
+                    chebyinf::cheby_order_0_a0,
+                    chebyinf::cheby_order_1_a0,
+                    X,
+                    Y
+                );
+            }
+            else
+            {
+                rxy = eval_chebyshev_fit(
+                    chebyinf::num_cheby_a1,
+                    chebyinf::cheby_coeff_a1,
+                    chebyinf::cheby_order_0_a1,
+                    chebyinf::cheby_order_1_a1,
+                    X,
+                    Y
+                );
+            }
+        }
+        else if (Y<=8.0)
+        {
+            if (X <= 8.0)
+            {
+                rxy = eval_chebyshev_fit(
+                    chebyinf::num_cheby_b0,
+                    chebyinf::cheby_coeff_b0,
+                    chebyinf::cheby_order_0_b0,
+                    chebyinf::cheby_order_1_b0,
+                    X,
+                    Y
+                );
+            }
+            else
+            {
+                rxy = eval_chebyshev_fit(
+                    chebyinf::num_cheby_b1,
+                    chebyinf::cheby_coeff_b1,
+                    chebyinf::cheby_order_0_b1,
+                    chebyinf::cheby_order_1_b1,
+                    X,
+                    Y
+                );
+            }
+        }
+        else
+        {
+            rxy = eval_chebyshev_fit(
+                chebyinf::num_cheby_c,
+                chebyinf::cheby_coeff_c,
+                chebyinf::cheby_order_0_c,
+                chebyinf::cheby_order_1_c,
+                X,
+                Y
+            );
+        }
+
+        // Add oscilatory bessel and struve terms to
+        // calculate full expint integral
+        cusfloat R = std::sqrt(pow2s(X)+pow2s(Y));
+        cusfloat f1 = 1/R - std::exp(-Y)/X + Y/pow3s(R)*rxy;
+        wave_term = 1/R - PI*std::exp(-Y)*(struve0(X)+bessely0(X)) - 2.0*f1;
+    }
+
+    return wave_term;
+}
+
+
+cusfloat wave_term_inf_depth_dx(cusfloat X, cusfloat Y)
+{
+    using namespace std;
+
+    cusfloat wave_term = 0.0;
+    if ((X>=8.0) && (Y>=20.0))
+    {
+        // Calculate formulation parameters
+        cusfloat R = std::sqrt(pow2s(X)+pow2s(Y));
+        cusfloat sf = Y/R;
+        cusfloat rfs = 1/R;
+        cusfloat rf0 = pow(rfs, 4.0);
+        cusfloat rf1 = pow(rfs, 3.0);
+        cusfloat xy = X*Y;
+        cusfloat pn = 1.0;
+        for (int i=0; i<=4; i++)
+        {
+            // Add new term to the series
+            wave_term += pn*(
+                -legendre_poly_der_raw(i, sf)*xy*rf0
+                -legendre_poly_raw(i, sf)*(i+1.0)*X*rf1
+            );
+
+
+            // Update formulation parameters
+            rf0 *= rfs;
+            rf1 *= rfs;
+            pn *= static_cast<cusfloat>(i+1);
+        }
+
+        wave_term = -X*pow3s(rfs) - PI*std::exp(-Y)*(2.0/PI-struve1(X)-bessely1(X))-2.0*wave_term;
     }
     else if (Y > 2*X)
     {
