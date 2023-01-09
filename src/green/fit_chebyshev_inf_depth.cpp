c4ea9691adecd4b49eb42868cfc8bfa5709bfced
sfruano <sergio.fernandezruano@unican.es>
2023-01-09 18:28:26 +0100

    Working with the optimization of number of coefficients in the Chebyshev  polynomials for the interpolation of the infinite water depth wave  term.

--- src/green/fit_chebyshev_inf_depth.cpp
+++ src/green/fit_chebyshev_inf_depth.cpp
@@ -24,6 +24,7 @@ namespace fs = std::filesystem;
 // Declare local module functions
 struct FitRegion;
 void generate_grid(FitRegion fr, double* x, double* y);
+void generate_grid_cheby(FitRegion fr, double* x, double* y);
 void get_x_range(double y, double &x0, double &x1, int side);
 void write_domain(std::ofstream &outfile, int num_cheby, double* cheby_coeff,
                 int* cheby_order_0, int* cheby_order_1, std::string domain_key);
@@ -54,7 +55,7 @@ struct FitRegion
 };
 
 
-double eval_chebyshev_xy(int n_order, FitRegion fr, double x, double y, double* cheby_coeff)
+double eval_chebyshev_xy(int n_order, FitRegion fr, double x, double y, double cheby_prec, double* cheby_coeff)
 {
     double sol = 0.0;
     double xl = 0.0, yl = 0.0;
@@ -62,8 +63,11 @@ double eval_chebyshev_xy(int n_order, FitRegion fr, double x, double y, double* 
     {
         for (int j=0; j<n_order; j++)
         {
-            fr.fit(x, y, xl, yl);
-            sol += cheby_coeff[i*n_order+j]*chebyshev_poly_raw(i, xl)*chebyshev_poly_raw(j, yl);
+            if (std::abs(cheby_coeff[i*n_order+j])>cheby_prec)
+            {
+                fr.fit(x, y, xl, yl);
+                sol += cheby_coeff[i*n_order+j]*chebyshev_poly_raw(i, xl)*chebyshev_poly_raw(j, yl);
+            }
         }
     }
 
@@ -115,7 +119,7 @@ void fit_chebyshev_xy(int n_order, int num_points, FitRegion fr, double* x, doub
 }
 
 
-void fit_sub_domain(FitRegion fr, int cheby_order, int &count_coeffs,
+void fit_sub_domain(FitRegion fr, int cheby_order, double cheby_thres, int &count_coeffs,
     double* cheby_coeff_filter, int* cheby_order_0, 
     int* cheby_order_1, std::string sub_domain_name)
 {
@@ -123,10 +127,13 @@ void fit_sub_domain(FitRegion fr, int cheby_order, int &count_coeffs,
     int cheby_order_2 = cheby_order*cheby_order;
     int num_points_p2 = fr.num_points*fr.num_points;
     double* x = generate_empty_vector<double>(num_points_p2);
+    double* xs = generate_empty_vector<double>(num_points_p2);
     double* y = generate_empty_vector<double>(num_points_p2);
+    double* ys = generate_empty_vector<double>(num_points_p2);
 
     // Generate grid
-    generate_grid(fr, x, y);
+    generate_grid(fr, xs, ys);
+    generate_grid_cheby(fr, x, y);
 
     // Generate target vector
     double* f = generate_empty_vector<double>(num_points_p2);
@@ -142,7 +149,7 @@ void fit_sub_domain(FitRegion fr, int cheby_order, int &count_coeffs,
     double* fxy = generate_empty_vector<double>(num_points_p2);
     for (int i=0; i<num_points_p2; i++)
     {
-        fxy[i] = wave_term_inf_depth_num(x[i], y[i]) + 1/std::sqrt(pow2s(x[i])+pow2s(y[i]));
+        fxy[i] = wave_term_inf_depth_num(xs[i], ys[i]) + 1/std::sqrt(pow2s(xs[i])+pow2s(ys[i]));
     }
 
     // Fit Chebyshev polynomials
@@ -155,9 +162,10 @@ void fit_sub_domain(FitRegion fr, int cheby_order, int &count_coeffs,
     double R = 0.0;
     for (int i=0; i<=num_points_p2; i++)
     {
-        R = std::sqrt(pow2s(x[i])+pow2s(y[i]));
-        f1 = 1/R - std::exp(-y[i])/x[i] + y[i]/pow3s(R)*eval_chebyshev_xy(cheby_order, fr, x[i], y[i], cheby_coeff);
-        f1 = 1/R - PI*std::exp(-y[i])*(struve0(x[i])+bessely0(x[i])) - 2.0*f1;
+        R = std::sqrt(pow2s(xs[i])+pow2s(ys[i]));
+        f1 = 1/R - std::exp(-ys[i])/xs[i] + ys[i]/pow3s(R)*eval_chebyshev_xy(cheby_order, fr, xs[i], ys[i],
+                                                                            cheby_thres, cheby_coeff);
+        f1 = 1/R - PI*std::exp(-ys[i])*(struve0(xs[i])+bessely0(xs[i])) - 2.0*f1;
         sol_fit[i] = f1;
     }
 
@@ -205,7 +213,6 @@ void fit_sub_domain(FitRegion fr, int cheby_order, int &count_coeffs,
     std::cout << std::endl;
 
     // Filter Chebyshev coefficients by threshold
-    double cheby_thres = 1e-6;
     for (int i=0; i<cheby_order_2; i++)
     {
         if (std::abs(cheby_coeff[i])>=cheby_thres)
@@ -270,6 +277,44 @@ void generate_grid(FitRegion fr, double* x, double* y)
 }
 
 
+void generate_grid_cheby(FitRegion fr, double* x, double* y)
+{
+    // Get Chebyshev polynomial roots
+    cusfloat* cheby_roots = generate_empty_vector<cusfloat>(fr.num_points);
+    chebyshev_poly_roots(fr.num_points, cheby_roots);
+
+    // Generate y coordinate grid
+    double dy = fr.y_max-fr.y_min;
+    double yi = 0.0;
+    for (int i=0; i<fr.num_points; i++)
+    {
+        yi = fr.y_min + (cheby_roots[i]+1)*dy/2.0;
+        for (int j=0; j<fr.num_points; j++)
+        {
+            y[i*fr.num_points+j] = yi;
+        }
+    }
+
+    // Generate x coordinate grid
+    double dx = 0.0;
+    double x0 = 0.0, x1 = 0.0;
+
+    for (int i=0; i<fr.num_points; i++)
+    {
+        get_x_range(y[i*fr.num_points], x0, x1, fr.side);
+        dx = x1-x0;
+        for (int j=0; j<fr.num_points; j++)
+        {
+            x[i*fr.num_points+j] = x0 + (cheby_roots[j]+1)*dx/2.0;
+        }
+    }
+
+    // Deallocate heap memory
+    mkl_free(cheby_roots);
+
+}
+
+
 void get_x_range(double y, double &x0, double &x1, int side)
 {
     if (y<=4.0)
@@ -318,7 +363,7 @@ int main(int argc, char* argv[])
 
     // Define fit properties
     int num_points = 100;
-    int cheby_order = 10;
+    int cheby_order = 20;
     int cheby_order_2 = cheby_order*cheby_order;
 
     // Allocate heap memory to storage fit results
@@ -345,57 +390,62 @@ int main(int argc, char* argv[])
 
     // Generate Chebyshev polynomials for region a0 (X<3.0 & 2.0<=Y<=4.0)
     std::string sub_domain_name_a0 = "Sub-Domain A0";
+    double cheby_thres_a0 = 1e-6;
     FitRegion fr_a0;
     fr_a0.num_points = num_points;
     fr_a0.side = 0;
     fr_a0.y_min = 2.0;
     fr_a0.y_max = 4.0;
     fr_a0.dy = (fr_a0.y_max-fr_a0.y_min);
-    fit_sub_domain(fr_a0, cheby_order, count_cheby_a0, cheby_coeff_filter_a0,
+    fit_sub_domain(fr_a0, cheby_order, cheby_thres_a0, count_cheby_a0, cheby_coeff_filter_a0,
         cheby_order_0_a0, cheby_order_1_a0, sub_domain_name_a0);
 
     // Generate Chebyshev polynomials for region a1 (X>3.0 & 2.0<=Y<=4.0)
     std::string sub_domain_name_a1 = "Sub-Domain A1";
+    double cheby_thres_a1 = 1e-5;
     FitRegion fr_a1;
     fr_a1.num_points = num_points;
     fr_a1.side = 1;
     fr_a1.y_min = 2.0;
     fr_a1.y_max = 4.0;
     fr_a1.dy = (fr_a1.y_max-fr_a1.y_min);
-    fit_sub_domain(fr_a1, cheby_order, count_cheby_a1, cheby_coeff_filter_a1,
+    fit_sub_domain(fr_a1, cheby_order, cheby_thres_a1, count_cheby_a1, cheby_coeff_filter_a1,
         cheby_order_0_a1, cheby_order_1_a1, sub_domain_name_a1);
 
     // Generate Chebyshev polynomials for region b0 (X<8.0 & 4.0<Y<=8.0)
     std::string sub_domain_name_b0 = "Sub-Domain B0";
+    double cheby_thres_b0 = 1e-5;
     FitRegion fr_b0;
     fr_b0.num_points = num_points;
     fr_b0.side = 0;
     fr_b0.y_min = 4.0001;
     fr_b0.y_max = 8.0;
     fr_b0.dy = (fr_b0.y_max-fr_b0.y_min);
-    fit_sub_domain(fr_b0, cheby_order, count_cheby_b0, cheby_coeff_filter_b0,
+    fit_sub_domain(fr_b0, cheby_order, cheby_thres_b0, count_cheby_b0, cheby_coeff_filter_b0,
         cheby_order_0_b0, cheby_order_1_b0, sub_domain_name_b0);
 
     // Generate Chebyshev polynomials for region b1 (X>8.0 & 4.0<Y<=8.0)
     std::string sub_domain_name_b1 = "Sub-Domain B1";
+    double cheby_thres_b1 = 1e-5;
     FitRegion fr_b1;
     fr_b1.num_points = num_points;
     fr_b1.side = 1;
     fr_b1.y_min = 4.0001;
     fr_b1.y_max = 8.0;
     fr_b1.dy = (fr_b1.y_max-fr_b1.y_min);
-    fit_sub_domain(fr_b1, cheby_order, count_cheby_b1, cheby_coeff_filter_b1,
+    fit_sub_domain(fr_b1, cheby_order, cheby_thres_b1, count_cheby_b1, cheby_coeff_filter_b1,
         cheby_order_0_b1, cheby_order_1_b1, sub_domain_name_b1);
     
     // Generate Chebyshev polynomials for region c (8.0<Y<=20.0)
     std::string sub_domain_name_c = "Sub-Domain C";
+    double cheby_thres_c = 1e-5;
     FitRegion fr_c;
     fr_c.num_points = num_points;
     fr_c.side = 1;
     fr_c.y_min = 8.00001;
     fr_c.y_max = 20.0;
     fr_c.dy = (fr_c.y_max-fr_c.y_min);
-    fit_sub_domain(fr_c, cheby_order, count_cheby_c, cheby_coeff_filter_c,
+    fit_sub_domain(fr_c, cheby_order, cheby_thres_c, count_cheby_c, cheby_coeff_filter_c,
         cheby_order_0_c, cheby_order_1_c, sub_domain_name_c);
 
     // Write module
