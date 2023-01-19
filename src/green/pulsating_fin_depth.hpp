b6140d8636d42f7cbe4ffce350db4fd3fe18d5d3
sfruano <sergio.fernandezruano@unican.es>
2023-01-19 18:15:24 +0100

    Added John series eigenfunction expansion to calculate the finite depth Green function at R/h > 1

---
+++ src/green/pulsating_fin_depth.hpp
@@ -0,0 +1,8 @@
+
+#ifndef __pulsating_hpp
+#define __pulsating_hpp
+
+cuscomplex jonh_series(cusfloat R, cusfloat z, cusfloat zeta, cusfloat h, cusfloat nu,
+                    cusfloat k0, int num_kn, cusfloat* kn);
+
+#endif
