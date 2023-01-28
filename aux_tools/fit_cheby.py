7d6bcdce72f61f4f2f2677258a09a64099150c6f
sfruano <sergio.fernandezruano@unican.es>
2023-01-28 17:35:13 +0100

    Added module to fit chebyshev polynomials to a target function.

---
+++ aux_tools/fit_cheby.py
@@ -0,0 +1,36 @@
+
+# Import general usage scientific libraries
+from numpy import ndarray, zeros
+from numpy.linalg import solve as np_solve
+from scipy.special import eval_chebyt
+
+
+def eval_chebyshev(x: ndarray, y: ndarray, n: int, c: ndarray)->ndarray:
+    sol = 0.0
+    for i in range(n):
+        for j in range(n):
+            sol += c[i*n+j]*eval_chebyt(i, x)*eval_chebyt(j, y)
+
+    return sol
+
+
+def eval_chebyshev_filter(x: ndarray, y: ndarray, c: ndarray, ncx: ndarray, ncy: ndarray)->ndarray:
+    sol = 0.0
+    for i in range(c.shape[0]):
+        sol += c[i]*eval_chebyt(ncx[i], x)*eval_chebyt(ncy[i], y)
+
+    return sol
+
+
+def fit_chebyshev(x: ndarray, y: ndarray, f: ndarray, n: int)->ndarray:
+    num_points = x.shape[0]
+    A = zeros((num_points, n*n))
+    for i in range(n):
+        for j in range(n):
+            A[:, i*n+j] = eval_chebyt(i, x)*eval_chebyt(j, y)
+
+    At = A.T.dot(A)
+    F = A.T.dot(f)
+    C = np_solve(At, F)
+
+    return C
