ccda89d903829ecbc2d59979ffc5405b5637b389
sfruano <sergio.fernandezruano@unican.es>
2023-01-31 14:37:27 +0100

    Rearranged module to be in alphabetical order.

--- aux_tools/fit_cheby.py
+++ aux_tools/fit_cheby.py
@@ -50,6 +50,82 @@ class FitProperties:
             return (y+1)*(self.y_max-self.y_min)/2 + self.y_min
 
 
+def eval_chebyshev_1d(x: ndarray, n: int, c: ndarray)->ndarray:
+    sol = 0.0
+    for i in range(n):
+        sol += c[i]*eval_chebyt(i, x)
+
+    return sol
+
+
+def eval_chebyshev_2d(x: ndarray, y: ndarray, n: int, c: ndarray)->ndarray:
+    sol = 0.0
+    for i in range(n):
+        for j in range(n):
+            sol += c[i*n+j]*eval_chebyt(i, x)*eval_chebyt(j, y)
+
+    return sol
+
+
+def eval_chebyshev_2d_filter(x: ndarray, y: ndarray, c: ndarray, ncx: ndarray, ncy: ndarray)->ndarray:
+    sol = 0.0
+    for i in range(c.shape[0]):
+        sol += c[i]*eval_chebyt(ncx[i], x)*eval_chebyt(ncy[i], y)
+
+    return sol
+
+
+def eval_chebyshev_3d_filter(x: ndarray, y: ndarray, z: ndarray, c: ndarray, ncx: ndarray, ncy: ndarray,
+                            ncz: ndarray)->ndarray:
+    sol = 0.0
+    for i in range(c.shape[0]):
+        sol += c[i]*eval_chebyt(ncx[i], x)*eval_chebyt(ncy[i], y)*eval_chebyt(ncz[i], z)
+
+    return sol
+
+
+def fit_chebyshev_1d(x: ndarray, f: ndarray, n: int)->ndarray:
+    num_points = x.shape[0]
+    A = zeros((num_points, n))
+    for i in range(n):
+            A[:, i] = eval_chebyt(i, x)
+
+    At = A.T.dot(A)
+    F = A.T.dot(f)
+    C = np_solve(At, F)
+
+    return C
+
+
+def fit_chebyshev_2d(x: ndarray, y: ndarray, f: ndarray, n: int)->ndarray:
+    num_points = x.shape[0]
+    A = zeros((num_points, n*n))
+    for i in range(n):
+        for j in range(n):
+            A[:, i*n+j] = eval_chebyt(i, x)*eval_chebyt(j, y)
+
+    At = A.T.dot(A)
+    F = A.T.dot(f)
+    C = np_solve(At, F)
+
+    return C
+
+
+def fit_chebyshev_3d(x: ndarray, y: ndarray, z: ndarray, f: ndarray, nx: int, ny: int, nz: int)->ndarray:
+    num_points = x.shape[0]
+    A = zeros((num_points, nx*ny*nz))
+    for i in range(nx):
+        for j in range(ny):
+            for k in range(nz):
+                A[:, i*(ny*nz)+j*nz+k] = eval_chebyt(i, x)*eval_chebyt(j, y)*eval_chebyt(k, z)
+
+    At = A.T.dot(A)
+    F = A.T.dot(f)
+    C = np_solve(At, F)
+
+    return C
+
+
 def fit_integral_2d(f_residual: Callable,
                 fit_props: FitProperties,
                 region_name: str,
@@ -202,79 +278,3 @@ def fit_integral_2d(f_residual: Callable,
         plt.show()
 
     return C_filter, NCX_filter, NCY_filter
-
-
-def eval_chebyshev_1d(x: ndarray, n: int, c: ndarray)->ndarray:
-    sol = 0.0
-    for i in range(n):
-        sol += c[i]*eval_chebyt(i, x)
-
-    return sol
-
-
-def eval_chebyshev_2d(x: ndarray, y: ndarray, n: int, c: ndarray)->ndarray:
-    sol = 0.0
-    for i in range(n):
-        for j in range(n):
-            sol += c[i*n+j]*eval_chebyt(i, x)*eval_chebyt(j, y)
-
-    return sol
-
-
-def eval_chebyshev_2d_filter(x: ndarray, y: ndarray, c: ndarray, ncx: ndarray, ncy: ndarray)->ndarray:
-    sol = 0.0
-    for i in range(c.shape[0]):
-        sol += c[i]*eval_chebyt(ncx[i], x)*eval_chebyt(ncy[i], y)
-
-    return sol
-
-
-def eval_chebyshev_3d_filter(x: ndarray, y: ndarray, z: ndarray, c: ndarray, ncx: ndarray, ncy: ndarray,
-                            ncz: ndarray)->ndarray:
-    sol = 0.0
-    for i in range(c.shape[0]):
-        sol += c[i]*eval_chebyt(ncx[i], x)*eval_chebyt(ncy[i], y)*eval_chebyt(ncz[i], z)
-
-    return sol
-
-
-def fit_chebyshev_1d(x: ndarray, f: ndarray, n: int)->ndarray:
-    num_points = x.shape[0]
-    A = zeros((num_points, n))
-    for i in range(n):
-            A[:, i] = eval_chebyt(i, x)
-
-    At = A.T.dot(A)
-    F = A.T.dot(f)
-    C = np_solve(At, F)
-
-    return C
-
-
-def fit_chebyshev_2d(x: ndarray, y: ndarray, f: ndarray, n: int)->ndarray:
-    num_points = x.shape[0]
-    A = zeros((num_points, n*n))
-    for i in range(n):
-        for j in range(n):
-            A[:, i*n+j] = eval_chebyt(i, x)*eval_chebyt(j, y)
-
-    At = A.T.dot(A)
-    F = A.T.dot(f)
-    C = np_solve(At, F)
-
-    return C
-
-
-def fit_chebyshev_3d(x: ndarray, y: ndarray, z: ndarray, f: ndarray, nx: int, ny: int, nz: int)->ndarray:
-    num_points = x.shape[0]
-    A = zeros((num_points, nx*ny*nz))
-    for i in range(nx):
-        for j in range(ny):
-            for k in range(nz):
-                A[:, i*(ny*nz)+j*nz+k] = eval_chebyt(i, x)*eval_chebyt(j, y)*eval_chebyt(k, z)
-
-    At = A.T.dot(A)
-    F = A.T.dot(f)
-    C = np_solve(At, F)
-
-    return C
