303d5995fac72704f7d4016dc1ab5bc2ee5a3b25
sfruano <sergio.fernandezruano@unican.es>
2023-02-01 17:13:53 +0100

    Added function to fit function with 3D Chebyshev polynomials. Also, it has been changed some attributes of FitProperties object.

--- aux_tools/fit_cheby.py
+++ aux_tools/fit_cheby.py
@@ -14,20 +14,36 @@ from scipy.special import eval_chebyt, roots_chebyt
 class FitProperties:
 
     def __init__(self) -> None:
-        self.num_x = 20
-        self.num_x_fit = 20
-        self.num_y = 20
-        self.num_y_fit = 20
+        self.cheby_order_x = 0
+        self.cheby_order_y = 0
+        self.cheby_order_z = 0
+        self.num_x = 30
+        self.num_x_fit = 30
+        self.num_y = 30
+        self.num_y_fit = 30
+        self.num_z = 30
+        self.num_z_fit = 30
         self.x_log_scale = False
         self.x_max = 0
-        self.x_map_fcn = None
+        self.x_map_fcn = lambda x: x
         self.x_min = 0.0
         self.y_log_scale = False
         self.y_max = 0.0
-        self.y_map_fcn = None
+        self.y_map_fcn = lambda y: y
         self.y_min = 0.0
-        self.cheby_order = 0
+        self.z_log_scale = False
+        self.z_max = 0.0
+        self.z_map_fcn = lambda z: z
+        self.z_min = 0.0
         self.cheby_tol = 0.0
+
+    def fit_points_to_order(self)->None:
+        self.num_x = self.cheby_order_x
+        self.num_x_fit = self.cheby_order_x
+        self.num_y = self.cheby_order_y
+        self.num_y_fit = self.cheby_order_y
+        self.num_z = self.cheby_order_z
+        self.num_z_fit = self.cheby_order_z
 
     def x_map(self, x: ndarray)->ndarray:
         return self.x_map_fcn(self.x_map_lin(x))
@@ -48,6 +64,16 @@ class FitProperties:
             return 10**((y+1)*(log10(self.y_max)-log10(self.y_min))/2 + log10(self.y_min))
         else:
             return (y+1)*(self.y_max-self.y_min)/2 + self.y_min
+        
+    def z_map(self, z: ndarray)->ndarray:
+        # return self.y_map_fcn(self.y_map_lin(y))
+        return self.z_map_fcn(z)
+    
+    def z_map_lin(self, z: ndarray)->ndarray:
+        if self.z_log_scale:
+            return 10**((z+1)*(log10(self.z_max)-log10(self.z_min))/2 + log10(self.z_min))
+        else:
+            return (z+1)*(self.z_max-self.z_min)/2 + self.z_min
 
 
 def eval_chebyshev_1d(x: ndarray, n: int, c: ndarray)->ndarray:
@@ -105,12 +131,12 @@ def fit_chebyshev_1d(x: ndarray, f: ndarray, n: int)->ndarray:
     return C
 
 
-def fit_chebyshev_2d(x: ndarray, y: ndarray, f: ndarray, n: int)->ndarray:
+def fit_chebyshev_2d(x: ndarray, y: ndarray, f: ndarray, nx: int, ny: int)->ndarray:
     num_points = x.shape[0]
-    A = zeros((num_points, n*n))
-    for i in range(n):
-        for j in range(n):
-            A[:, i*n+j] = eval_chebyt(i, x)*eval_chebyt(j, y)
+    A = zeros((num_points, nx*ny))
+    for i in range(nx):
+        for j in range(ny):
+            A[:, i*ny+j] = eval_chebyt(i, x)*eval_chebyt(j, y)
 
     At = A.T.dot(A)
     F = A.T.dot(f)
@@ -247,7 +273,8 @@ def fit_integral_2d(f_residual: Callable,
     x_min = fit_props.x_min
     y_max = fit_props.y_max
     y_min = fit_props.y_min
-    cheby_order = fit_props.cheby_order
+    cheby_order_x = fit_props.cheby_order_x
+    cheby_order_y = fit_props.cheby_order_y
     cheby_tol = fit_props.cheby_tol
 
     # Define parametric space for function fit
@@ -267,11 +294,12 @@ def fit_integral_2d(f_residual: Callable,
             Ff[i, j] = f_residual(x_fit[i], y_fit[j])
 
     # Calculate polynomial fit coefficients
-    C = fit_chebyshev_2d(Xfp.ravel(), Yfp.ravel(), Ff.ravel(), cheby_order)
+    C = fit_chebyshev_2d(Xfp.ravel(), Yfp.ravel(), Ff.ravel(), cheby_order_x, cheby_order_y)
     
     # Filter coefficients to the precision required
-    n_cheby = linspace(0, cheby_order-1, cheby_order, dtype=int)
-    NCX,NCY = meshgrid(n_cheby, n_cheby, indexing="ij")
+    n_cheby_x = linspace(0, cheby_order_x-1, cheby_order_x, dtype=int)
+    n_cheby_y = linspace(0, cheby_order_y-1, cheby_order_y, dtype=int)
+    NCX,NCY = meshgrid(n_cheby_x, n_cheby_y, indexing="ij")
     pos = np_abs(C) > cheby_tol
     C_filter = C[pos]
     NCX_filter = (NCX.ravel())[pos]
@@ -373,18 +401,111 @@ def fit_integral_2d(f_residual: Callable,
     ax5.set_ylabel("Y")
     plt.colorbar(cntf, ax=ax5)
 
-    cheby_order_ticks = linspace(0, cheby_order-1, cheby_order)
+    cheby_order_x_ticks = linspace(0, cheby_order_x-1, cheby_order_x)
+    cheby_order_y_ticks = linspace(0, cheby_order_y-1, cheby_order_y)
     fig = plt.figure()
     fig.suptitle("Fit coefficient matrix")
     ax = fig.add_subplot(111)
-    imc = ax.imshow(log10(C.reshape(cheby_order, cheby_order).T), origin="lower")
+    imc = ax.imshow(log10(C.reshape(cheby_order_x, cheby_order_y).T), origin="lower")
     ax.set_xlabel("X")
-    ax.set_xticks(cheby_order_ticks)
+    ax.set_xticks(cheby_order_x_ticks)
     ax.set_ylabel("Y")
-    ax.set_yticks(cheby_order_ticks)
+    ax.set_yticks(cheby_order_y_ticks)
     plt.colorbar(imc, ax=ax)
 
     if show_figs:
         plt.show()
 
     return C_filter, NCX_filter, NCY_filter
+
+
+def fit_integral_3d(f_residual: Callable,
+                fit_props: FitProperties,
+                region_name: str,
+                )->None:
+    # Define parametric space limits
+    num_cross_sections = 5
+    x_max = fit_props.x_max
+    x_min = fit_props.x_min
+    y_max = fit_props.y_max
+    y_min = fit_props.y_min
+    z_max = fit_props.z_max
+    z_min = fit_props.z_min
+    cheby_order_x = fit_props.cheby_order_x
+    cheby_order_y = fit_props.cheby_order_y
+    cheby_order_z = fit_props.cheby_order_z
+    cheby_tol = fit_props.cheby_tol
+
+    # Define parametric space for function fit
+    num_x_fit = fit_props.num_x_fit
+    num_y_fit = fit_props.num_y_fit
+    num_z_fit = fit_props.num_z_fit
+    x_fit_poly = roots_chebyt(num_x_fit)[0]
+    y_fit_poly = roots_chebyt(num_y_fit)[0]
+    z_fit_poly = roots_chebyt(num_z_fit)[0]
+    x_fit = fit_props.x_map_lin(x_fit_poly)
+    y_fit = fit_props.y_map_lin(y_fit_poly)
+    z_fit = fit_props.z_map_lin(z_fit_poly)
+    Xfp,Yfp,Zfp = meshgrid(x_fit_poly, y_fit_poly, z_fit_poly, indexing="ij")
+    Xf,Yf,Zf = meshgrid(x_fit, y_fit, z_fit, indexing="ij")
+
+    # Calculate residual function over the fit points
+    Ff = zeros((num_x_fit, num_y_fit, num_z_fit))
+    for i in range(num_x_fit):
+        for j in range(num_y_fit):
+            for k in range(num_z_fit):
+                Ff[i, j, k] = f_residual(x_fit[i], y_fit[j], z_fit[k])
+
+    # Calculate polynomial fit coefficients
+    C = fit_chebyshev_3d(Xfp.ravel(), Yfp.ravel(), Zfp.ravel(), Ff.ravel(), cheby_order_x, cheby_order_y, cheby_order_z)
+    
+    # Filter coefficients to the precision required
+    n_cheby_x = linspace(0, cheby_order_x-1, cheby_order_x, dtype=int)
+    n_cheby_y = linspace(0, cheby_order_y-1, cheby_order_y, dtype=int)
+    n_cheby_z = linspace(0, cheby_order_z-1, cheby_order_z, dtype=int)
+    NCX,NCY,NCZ = meshgrid(n_cheby_x, n_cheby_y, n_cheby_z, indexing="ij")
+    pos = np_abs(C) > cheby_tol
+    C_filter = C[pos]
+    NCX_filter = (NCX.ravel())[pos]
+    NCY_filter = (NCY.ravel())[pos]
+    NCZ_filter = (NCZ.ravel())[pos]
+
+    # Define parametric space
+    num_x = fit_props.num_x
+    num_y = fit_props.num_y
+    num_z = fit_props.num_z
+    xe = linspace(-1.0, 1.0, num_x)
+    ye = linspace(-1.0, 1.0, num_y)
+    ze = linspace(-1.0, 1.0, num_z)
+    x = fit_props.x_map_lin(xe)
+    y = fit_props.y_map_lin(ye)
+    z = fit_props.z_map_lin(ze)
+    Xe,Ye,Ze = meshgrid(xe, ye, ze, indexing="ij")
+    X,Y,Z = meshgrid(fit_props.x_map_fcn(x), fit_props.y_map_fcn(y), fit_props.z_map_fcn(z), indexing="ij")
+    X0 = x_min
+    Y0 = y_min
+    Z0 = z_min
+
+    # Calculate residual function over the evaluation points
+    Fr = zeros((num_x, num_y, num_z))
+    for i in range(num_x):
+        for j in range(num_y):
+            for k in range(num_z):
+                Fr[i, j, k] = f_residual(x[i], y[j], z[k])
+    
+    Ffe = eval_chebyshev_3d_filter(Xe.ravel(), Ye.ravel(), Ze.ravel(), C_filter, NCX_filter, NCY_filter, NCZ_filter)
+    Ffe = Ffe.reshape(num_x, num_y, num_z)
+
+    # Print error statistics
+    err_tol = 1e-6
+    err_abs = np_abs(Ffe-Fr)
+    pos = err_abs > err_tol
+    err_over_thr = pos.sum()/err_abs.shape[0]/err_abs.shape[1]/err_abs.shape[2]*100
+    print(f"Error Statistics - {region_name}:")
+    print(f" -> Maximum: {err_abs.max()}")
+    print(f" -> Minimum: {err_abs.min()}")
+    print(f" -> Mean: {err_abs.mean()}")
+    print(f" -> Values over threshold [{err_tol:0.1E}]: {err_over_thr} %")
+    print(f" -> Num. Coeffs: {C.shape[0]} - Num. Coeffs Filter: {C_filter.shape[0]}")
+
+    return C_filter, NCX_filter, NCY_filter, NCZ_filter
