de8d2dd120e7dc624d7245dcc9ec843e929cd256
sfruano <sergio.fernandezruano@unican.es>
2023-02-03 17:07:22 +0100

    Removed const keyword in order to make the data modificable after object creation

--- aux_tools/generate_findepth_coeffs_mod.py
+++ aux_tools/generate_findepth_coeffs_mod.py
@@ -34,8 +34,8 @@ def generate_coeffs_modules(database_path: str, file_path: str, int_name: str)->
     intervals_bounds = fid_db["intervals_bounds"][:]
     num_intervals = intervals_bounds.shape[0]-1
     interval_str = ", ".join(f"{i:0.6E}" for i in intervals_bounds)
-    fid.writelines(str_start_col("    const", f"int num_intervals = {num_intervals:d};\n", REF_COL))
-    fid.writelines(str_start_col("    const", f"cusfloat interval_bounds[{intervals_bounds.shape[0]:d}] = " + "{" + interval_str + "};\n\n", REF_COL))
+    fid.writelines(str_start_col("    int", f"num_intervals = {num_intervals:d};\n", REF_COL))
+    fid.writelines(str_start_col("    cusfloat", f"interval_bounds[{intervals_bounds.shape[0]:d}] = " + "{" + interval_str + "};\n\n", REF_COL))
 
     # Save number of points
     max_size_fold = fid_db["max_size_fold"][()]
@@ -43,9 +43,9 @@ def generate_coeffs_modules(database_path: str, file_path: str, int_name: str)->
     num_points_cum = fid_db["num_points_cum"][:]
     num_points_str = ", ".join(f"{i:d}" for i in num_points)
     num_points_cum_str = ", ".join(f"{i:d}" for i in num_points_cum)
-    fid.writelines(str_start_col("    const", f"int num_points[{num_points.shape[0]}] = " + "{" + num_points_str + "};\n", REF_COL))
-    fid.writelines(str_start_col("    const", f"int num_points_cum[{num_points_cum.shape[0]}] = " + "{" + num_points_cum_str + "};\n", REF_COL))
-    fid.writelines(str_start_col("    const", f"int max_size_fold = {max_size_fold:d};\n\n", REF_COL))
+    fid.writelines(str_start_col("    int", f"num_points[{num_points.shape[0]}] = " + "{" + num_points_str + "};\n", REF_COL))
+    fid.writelines(str_start_col("    int", f"num_points_cum[{num_points_cum.shape[0]}] = " + "{" + num_points_cum_str + "};\n", REF_COL))
+    fid.writelines(str_start_col("    int", f"max_size_fold = {max_size_fold:d};\n\n", REF_COL))
 
     # Save interval bounds
     zeros_map = zeros((num_intervals, ))
@@ -73,27 +73,27 @@ def generate_coeffs_modules(database_path: str, file_path: str, int_name: str)->
 
     # Write chebyshev polynomials
     cheby_coeffs = interval_to_vector(fid_db, "cheby_coeffs", intervals_bounds.shape[0]-1)
-    fid.writelines(str_start_col("    const int", f"num_c = {cheby_coeffs.shape[0]};\n", REF_COL))
-    fid.writelines(str_start_col("    const cusfloat", f"c[{cheby_coeffs.shape[0]}] = " + "{\n", REF_COL))
+    fid.writelines(str_start_col("    int", f"num_c = {cheby_coeffs.shape[0]};\n", REF_COL))
+    fid.writelines(str_start_col("    cusfloat", f"c[{cheby_coeffs.shape[0]}] = " + "{\n", REF_COL))
     for i, iv in enumerate(cheby_coeffs):
         fid.writelines(f"                                   {iv:0.16E},  // C[{i}]\n")
     fid.writelines(f"                                " + "};\n")
     cf = zeros((max_size_fold, ))
-    write_vector(fid, cf, "cf")
+    write_vector(fid, cf, "cf", "cusfloat")
 
     # Write polynomials coefficients
     ncx = interval_to_vector(fid_db, "ncx", num_intervals)
     ncxf = zeros((max_size_fold, ))
-    write_vector(fid, ncx, "ncx")
-    write_vector(fid, ncxf, "ncxf")
+    write_vector(fid, ncx, "ncx", "int")
+    write_vector(fid, ncxf, "ncxf", "int")
     if dims >= 2:
         ncy = interval_to_vector(fid_db, "ncy", num_intervals)
         ncyf = zeros((max_size_fold, ))
-        write_vector(fid, ncy, "ncy")
-        write_vector(fid, ncyf, "ncyf")
+        write_vector(fid, ncy, "ncy", "int")
+        write_vector(fid, ncyf, "ncyf", "int")
         if dims == 3:
             ncz = interval_to_vector(fid_db, "ncz", num_intervals)
-            write_vector(fid, ncz, "ncz")
+            write_vector(fid, ncz, "ncz", "int")
         else:
             raise ValueError(f"Number of dimensions: {dims} not available.")
 
@@ -170,11 +170,11 @@ def write_interval_bounds(fid, fid_db, field_name: str, num_intervals: int, var_
         field_data_str = ", ".join(f"{int(i):d}" for i in field_data)
     else:
         raise ValueError("Variable type not recognized!.")
-    fid.writelines(str_start_col(f"    const {var_type}", f"{field_name}[{field_data.shape[0]}] = " + "{" + field_data_str + "};\n", REF_COL))
+    fid.writelines(str_start_col(f"    {var_type}", f"{field_name}[{field_data.shape[0]}] = " + "{" + field_data_str + "};\n", REF_COL))
 
 
-def write_vector(fid, field: ndarray, field_tag: str)->None:
-    fid.writelines(str_start_col("    const int", f"{field_tag}[{field.shape[0]}] = " + "{\n", REF_COL))
+def write_vector(fid, field: ndarray, field_tag: str, var_type: str)->None:
+    fid.writelines(str_start_col(f"    {var_type}", f"{field_tag}[{field.shape[0]}] = " + "{\n", REF_COL))
     for i, iv in enumerate(field):
         fid.writelines(f"                                   {int(iv):d},  // {field_tag}[{i}]\n")
     fid.writelines(f"                            " + "};\n")
