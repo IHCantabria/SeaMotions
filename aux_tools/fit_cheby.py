f1e1c59fffe35d4dd793c32f1faca7e8dc5697e4
sfruano <sergio.fernandezruano@unican.es>
2023-02-15 17:25:37 +0100

    Modified module in order to output fit statistics. Also, changed the behaviour of the plot funciton.

--- aux_tools/fit_cheby.py
+++ aux_tools/fit_cheby.py
@@ -5,7 +5,7 @@ from typing import Callable
 # Import general usage scientific libraries
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
-from numpy import linspace, log10, meshgrid, ndarray, zeros
+from numpy import linspace, log10, meshgrid, ndarray, sqrt, zeros
 from numpy import abs as np_abs
 from numpy.linalg import solve as np_solve
 from scipy.special import eval_chebyt, roots_chebyt
@@ -74,6 +74,17 @@ class FitProperties:
             return 10**((z+1)*(log10(self.z_max)-log10(self.z_min))/2 + log10(self.z_min))
         else:
             return (z+1)*(self.z_max-self.z_min)/2 + self.z_min
+
+
+class FitStats:
+
+    def __init__(self) -> None:
+        self.err_over_thr = 0
+        self.err_tol = 1e-6
+        self.max_err = 0.0
+        self.mean_err = 0.0
+        self.min_err = 0.0
+        self.num_coeffs = 0
 
 
 def eval_chebyshev_1d(x: ndarray, n: int, c: ndarray)->ndarray:
@@ -293,8 +304,16 @@ def fit_integral_2d(f_residual: Callable,
         for j in range(num_y_fit):
             Ff[i, j] = f_residual(x_fit[i], y_fit[j])
 
+    X2 = Xfp.flatten()
+    Y2 = Yfp.flatten()
+    F2 = Ff.flatten()
+    # R = sqrt(X2**2.0+Y2**2.0)
+    # pos = R > 1e-11
+    # X2 = X2[pos]
+    # Y2 = Y2[pos]
+    # F2 = F2[pos]
     # Calculate polynomial fit coefficients
-    C = fit_chebyshev_2d(Xfp.ravel(), Yfp.ravel(), Ff.ravel(), cheby_order_x, cheby_order_y)
+    C = fit_chebyshev_2d(X2, Y2, F2, cheby_order_x, cheby_order_y)
     
     # Filter coefficients to the precision required
     n_cheby_x = linspace(0, cheby_order_x-1, cheby_order_x, dtype=int)
@@ -323,7 +342,7 @@ def fit_integral_2d(f_residual: Callable,
         for j in range(num_y):
             Fr[i, j] = f_residual(x[i], y[j])
     
-    Ffe = eval_chebyshev_2d_filter(Xe.ravel(), Ye.ravel(), C_filter, NCX_filter, NCY_filter)
+    Ffe = eval_chebyshev_2d_filter(Xe, Ye, C_filter, NCX_filter, NCY_filter)
     Ffe = Ffe.reshape(num_x, num_y)
     
     y0 = linspace(fit_props.y_min, fit_props.y_max, num_cross_sections)
@@ -338,85 +357,94 @@ def fit_integral_2d(f_residual: Callable,
         for j in range(num_y):
             FX0[i, j] = f_residual(x0[i], y[j])
 
-    # Print error statistics
+    # Calculate and storage fit statistics
     err_tol = 1e-6
     err_abs = np_abs(Ffe-Fr)
     pos = err_abs > err_tol
     err_over_thr = pos.sum()/err_abs.shape[0]/err_abs.shape[1]*100
-    print("Error Statistics:")
-    print(f" -> Maximum: {err_abs.max()}")
-    print(f" -> Minimum: {err_abs.min()}")
-    print(f" -> Mean: {err_abs.mean()}")
-    print(f" -> Values over threshold [{err_tol:0.1E}]: {err_over_thr} %")
+    fit_stats = FitStats()
+    fit_stats.err_tol = err_tol
+    fit_stats.max_err = err_abs.max()
+    fit_stats.mean_err = err_abs.mean()
+    fit_stats.min_err = err_abs.min()
+    fit_stats.err_over_thr = err_over_thr
+    fit_stats.num_coeffs = C_filter.shape[0]
+
+    # Print error statistics
+    print(f"ERROR STATISTICS - REGION: {region_name}")
+    print(f" -> Maximum: {fit_stats.max_err}")
+    print(f" -> Minimum: {fit_stats.min_err}")
+    print(f" -> Mean: {fit_stats.mean_err}")
+    print(f" -> Values over threshold [{err_tol:0.1E}]: {fit_stats.err_over_thr} %")
+    print("\n")
 
     # Plot residual function
-    fig = plt.figure()
-    fig.suptitle(region_name + f" - Total Coeffs: {C_filter.shape[0]:d}")
-    ax0 = fig.add_subplot(231)
-    ax1 = fig.add_subplot(232, projection='3d')
-    ax4 = fig.add_subplot(233)
-    ax2 = fig.add_subplot(234)
-    ax3 = fig.add_subplot(235)
-    ax5 = fig.add_subplot(236)
-
-    cfx = ax0.contourf(X, Y, Fr)
-    ax0.set_xlabel("X")
-    ax0.set_ylabel("Y")
-    if fit_props.x_log_scale:
-        ax0.set_xscale("log")
-    if fit_props.y_log_scale:
-        ax0.set_yscale("log")
-    plt.colorbar(cfx, ax=ax0)
-
-    psf = ax1.plot_surface(X, Y, Fr, cmap="jet")
-    ax1.set_xlabel("X")
-    ax1.set_ylabel("Y")
-    plt.colorbar(psf, ax=ax1)
-
-    ax2.set_title(f"Y = {y_min:0.1E}")
-    for i in range(num_cross_sections):
-        ax2.plot(fit_props.x_map_fcn(x), FY0[i, :], label=f"Y = {y0[i]:0.2E}")
-    ax2.legend()
-    if fit_props.x_log_scale:
-        ax2.set_xscale("log")
-    ax2.set_xlabel("X")
-
-    ax3.set_title(f"X = {x_min:0.1E}")
-    for i in range(num_cross_sections):
-        ax3.plot(fit_props.y_map_fcn(y), FX0[i, :], label=f"X = {x0[i]:0.2E}")
-    ax3.legend()
-    if fit_props.y_log_scale:
-        ax3.set_xscale("log")
-    ax3.set_xlabel("Y")
-
-    ax4.set_title("log10(|Ffe-Fr|)")
-    cntf = ax4.contourf(X, Y, log10(err_abs), cmap="jet")
-    ax4.set_xlabel("X")
-    ax4.set_ylabel("Y")
-    plt.colorbar(cntf, ax=ax4)
-
-    ax5.set_title("log10(|Ffe-Fr|/|Fr|)")
-    cntf = ax5.contourf(X, Y, log10(np_abs(Ffe-Fr)/np_abs(Fr)))
-    ax5.set_xlabel("X")
-    ax5.set_ylabel("Y")
-    plt.colorbar(cntf, ax=ax5)
-
-    cheby_order_x_ticks = linspace(0, cheby_order_x-1, cheby_order_x)
-    cheby_order_y_ticks = linspace(0, cheby_order_y-1, cheby_order_y)
-    fig = plt.figure()
-    fig.suptitle("Fit coefficient matrix")
-    ax = fig.add_subplot(111)
-    imc = ax.imshow(log10(C.reshape(cheby_order_x, cheby_order_y).T), origin="lower")
-    ax.set_xlabel("X")
-    ax.set_xticks(cheby_order_x_ticks)
-    ax.set_ylabel("Y")
-    ax.set_yticks(cheby_order_y_ticks)
-    plt.colorbar(imc, ax=ax)
-
     if show_figs:
+        fig = plt.figure()
+        fig.suptitle(region_name + f" - Total Coeffs: {C_filter.shape[0]:d}")
+        ax0 = fig.add_subplot(231)
+        ax1 = fig.add_subplot(232, projection='3d')
+        ax4 = fig.add_subplot(233)
+        ax2 = fig.add_subplot(234)
+        ax3 = fig.add_subplot(235)
+        ax5 = fig.add_subplot(236)
+
+        cfx = ax0.contourf(X, Y, Fr)
+        ax0.set_xlabel("X")
+        ax0.set_ylabel("Y")
+        if fit_props.x_log_scale:
+            ax0.set_xscale("log")
+        if fit_props.y_log_scale:
+            ax0.set_yscale("log")
+        plt.colorbar(cfx, ax=ax0)
+
+        psf = ax1.plot_surface(X, Y, Fr, cmap="jet")
+        ax1.set_xlabel("X")
+        ax1.set_ylabel("Y")
+        plt.colorbar(psf, ax=ax1)
+
+        ax2.set_title(f"Y = {y_min:0.1E}")
+        for i in range(num_cross_sections):
+            ax2.plot(fit_props.x_map_fcn(x), FY0[i, :], label=f"Y = {y0[i]:0.2E}")
+        ax2.legend()
+        if fit_props.x_log_scale:
+            ax2.set_xscale("log")
+        ax2.set_xlabel("X")
+
+        ax3.set_title(f"X = {x_min:0.1E}")
+        for i in range(num_cross_sections):
+            ax3.plot(fit_props.y_map_fcn(y), FX0[i, :], label=f"X = {x0[i]:0.2E}")
+        ax3.legend()
+        if fit_props.y_log_scale:
+            ax3.set_xscale("log")
+        ax3.set_xlabel("Y")
+
+        ax4.set_title("log10(|Ffe-Fr|)")
+        cntf = ax4.contourf(X, Y, log10(err_abs), cmap="jet")
+        ax4.set_xlabel("X")
+        ax4.set_ylabel("Y")
+        plt.colorbar(cntf, ax=ax4)
+
+        ax5.set_title("log10(|Ffe-Fr|/|Fr|)")
+        cntf = ax5.contourf(X, Y, log10(np_abs(Ffe-Fr)/np_abs(Fr)))
+        ax5.set_xlabel("X")
+        ax5.set_ylabel("Y")
+        plt.colorbar(cntf, ax=ax5)
+
+        cheby_order_x_ticks = linspace(0, cheby_order_x-1, cheby_order_x)
+        cheby_order_y_ticks = linspace(0, cheby_order_y-1, cheby_order_y)
+        fig = plt.figure()
+        fig.suptitle("Fit coefficient matrix")
+        ax = fig.add_subplot(111)
+        imc = ax.imshow(log10(C.reshape(cheby_order_x, cheby_order_y).T), origin="lower")
+        ax.set_xlabel("X")
+        ax.set_xticks(cheby_order_x_ticks)
+        ax.set_ylabel("Y")
+        ax.set_yticks(cheby_order_y_ticks)
+        plt.colorbar(imc, ax=ax)
         plt.show()
 
-    return C_filter, NCX_filter, NCY_filter
+    return C_filter, NCX_filter, NCY_filter, fit_stats, fit_props
 
 
 def fit_integral_3d(f_residual: Callable,
