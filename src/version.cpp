22ddbfe4e6fd89122ad463c49c374c22540a07cd
sfruano <sergio.fernandezruano@unican.es>
2023-07-26 16:45:16 +0200

    Deleted version source file (.cpp).

--- src/version.cpp
+++
@@ -1,5 +0,0 @@
-//the path to the version.h file is relative to your build directory.
-#include "../src/version.hpp"
-
-const std::string gVERSION = "v0.0.0-d";
-const std::string gVERSION_SHORT = "0.0.0";
