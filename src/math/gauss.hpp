f40255efc20194165edbc563ba2780c42ad90471
sfruano <sergio.fernandezruano@unican.es>
2023-08-17 18:31:05 +0200

    Added container for the gauss points in order to transport them across the code in an cleaner way.

--- src/math/gauss.hpp
+++ src/math/gauss.hpp
@@ -2,10 +2,59 @@
 #ifndef __gauss_hpp
 #define __gauss_hpp
 
+// Include general usage scientific libraries
+#include "mkl.h"
+
 // Include local modules
 #include "../config.hpp"
+#include "math_tools.hpp"
 
-void get_gauss_legendre(int num_points, cusfloat* weights, cusfloat* points);
 
+/**************************************/
+/********* Declare functions***********/
+/**************************************/
+void get_gauss_legendre(
+                            int         num_points, 
+                            cusfloat*   roots, 
+                            cusfloat*   weights
+                        );
+
+
+/**************************************/
+/********* Declare classes ************/
+/**************************************/
+struct GaussPoints
+{
+    // Define local variables
+    int         np          = 0;
+    cusfloat*   roots       = nullptr;
+    cusfloat*   weights     = nullptr;
+
+    // Define constructors and destructor
+    GaussPoints( int np_in )
+    {
+        // Storage input data
+        this->np        = np_in;
+
+        // Allocate space for roots and weights
+        this->roots     = generate_empty_vector<cusfloat>( this->np );
+        this->weights   = generate_empty_vector<cusfloat>( this->np );
+
+        // Get the required number of gauss points
+        get_gauss_legendre(
+                                this->np,
+                                this->roots,
+                                this->weights
+                            );
+        
+    }
+
+    ~GaussPoints( void )
+    {
+        mkl_free( this->roots );
+        mkl_free( this->weights );
+    }
+    
+};
 
 #endif
