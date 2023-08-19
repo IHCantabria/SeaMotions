b0c74c9c200ae19b4287e377ee4def2e55f1a28c
sfruano <sergio.fernandezruano@unican.es>
2023-08-19 12:16:41 +0200

    Added macro check statement to define the compilation of a MPI program.

--- src/config.hpp
+++ src/config.hpp
@@ -14,12 +14,24 @@ constexpr bool      _DEBUG_BUILD        = false;
 #ifdef SIMPLE_PREC
 typedef float cusfloat;
 typedef std::complex<float> cuscomplex;
+
+    #ifdef _MPI_BUILD
+    #include "mpi.h"
+    typedef MPI_FLOAT mpi_float;
+    #endif
+
 constexpr int FLOATING_PRECISION = 32;
 constexpr cusfloat EPS_PRECISION = 1e-6;
 constexpr cusfloat EPS_PRECISION_ORDER = -6;
 #else
 typedef double cusfloat;
 typedef std::complex<double> cuscomplex;
+
+    #ifdef _MPI_BUILD
+    #include "mpi.h"
+    typedef MPI_DOUBLE mpi_double;
+    #endif
+
 constexpr int FLOATING_PRECISION = 32;
 constexpr cusfloat EPS_PRECISION = 1e-14;
 constexpr cusfloat EPS_PRECISION_ORDER = -14;
