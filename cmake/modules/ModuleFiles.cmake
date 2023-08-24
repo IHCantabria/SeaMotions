5afb118851c0e9c7851ae36bde797d57639879cb
sfruano <sergio.fernandezruano@unican.es>
2023-08-24 16:37:06 +0200

    Added body_def to the container module files.

--- cmake/modules/ModuleFiles.cmake
+++ cmake/modules/ModuleFiles.cmake
@@ -1,6 +1,8 @@
 # Pack containers class definition
 set(
         containers_module_files
+        ${CMAKE_SOURCE_DIR}/src/containers/body_def.hpp
+        ${CMAKE_SOURCE_DIR}/src/containers/body_def.cpp
         ${CMAKE_SOURCE_DIR}/src/containers/containers.hpp
         ${CMAKE_SOURCE_DIR}/src/containers/panel_geom.hpp
         ${CMAKE_SOURCE_DIR}/src/containers/panel_geom.cpp
