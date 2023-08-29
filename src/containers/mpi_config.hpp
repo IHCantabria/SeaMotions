6750be21349aca86065eeff65f93da300432f8d8
sfruano <sergio.fernandezruano@unican.es>
2023-08-29 12:40:21 +0200

    Added method to check if the current process is the root one.

--- src/containers/mpi_config.hpp
+++ src/containers/mpi_config.hpp
@@ -5,9 +5,16 @@
 struct MpiConfig
 {
 public:
+    // Define class attributes
     int proc_rank   = 0;
     int procs_total = 0;
     int proc_root   = 0;
+
+    // Define class methods
+    bool is_root( void )
+    {
+        return this->proc_root == this->proc_rank;
+    }
 
 };
 
