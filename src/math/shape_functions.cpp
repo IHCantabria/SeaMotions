b944e599e5ec375b809cfb0406dea488e4a5af0f
sfruano <sergio.fernandezruano@unican.es>
2023-09-12 09:15:27 +0200

    Added if statement to hold the order zero case.

--- src/math/shape_functions.cpp
+++ src/math/shape_functions.cpp
@@ -1,4 +1,7 @@
 
+// Include general usage libraries
+#include <iostream>
+
 // Include local modules
 #include "shape_functions.hpp"
 
@@ -7,7 +10,23 @@ int     dofs_rectangular_region(
                                     int poly_order
                                 )
 {
-    return poly_order * poly_order;
+    int dofs_total = 0;
+    if ( poly_order == 0 )
+    {
+        dofs_total = 1;
+    }
+    else if ( poly_order > 0 )
+    {
+        dofs_total = ( poly_order + 1 ) * ( poly_order + 1 );
+    }
+    else
+    {
+        std::cerr << "ERROR - dofs_rectangular_region" << std::endl;
+        std::cerr << "Polynomial order: " << poly_order << " not valid." << std::endl;
+        throw std::runtime_error( "" );
+    }
+
+    return dofs_total;
 }
 
 
@@ -16,9 +35,22 @@ int     dofs_triangular_region(
                                 )
 {
     int dofs_total = 0;
-    for ( int i=1; i<poly_order+1; i++ )
+    if ( poly_order == 0 )
     {
-        dofs_total += i;
+        dofs_total = 1;
+    }
+    else if ( poly_order > 0 )
+    {
+        for ( int i=1; i<poly_order+2; i++ )
+        {
+            dofs_total += i;
+        }
+    }
+    else
+    {
+        std::cerr << "ERROR - dofs_rectangular_region" << std::endl;
+        std::cerr << "Polynomial order: " << poly_order << " not valid." << std::endl;
+        throw std::runtime_error( "" );
     }
 
     return dofs_total;
