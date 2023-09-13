2a7e3d52de902fdf40522a7d9b03562c4d2dcac6
sfruano <sergio.fernandezruano@unican.es>
2023-09-13 10:58:07 +0200

    Renamed module green_interaces to interfaces. This change tries to give a more general purpose to the module.

--- cmake/modules/ModuleFiles.cmake
+++ cmake/modules/ModuleFiles.cmake
@@ -101,11 +101,11 @@ set(
 
 # Pack Green function interfaces module files
 set(
-        green_interfaces_module_files
-        ${CMAKE_SOURCE_DIR}/src/green_interfaces/gwf_interface.hpp
-        ${CMAKE_SOURCE_DIR}/src/green_interfaces/gwf_interface.cpp
-        ${CMAKE_SOURCE_DIR}/src/green_interfaces/gwfdn_interface.hpp
-        ${CMAKE_SOURCE_DIR}/src/green_interfaces/gwfdn_interface.cpp
+        interfaces_module_files
+        ${CMAKE_SOURCE_DIR}/src/interfaces/gwf_interface.hpp
+        ${CMAKE_SOURCE_DIR}/src/interfaces/gwf_interface.cpp
+        ${CMAKE_SOURCE_DIR}/src/interfaces/gwfdn_interface.hpp
+        ${CMAKE_SOURCE_DIR}/src/interfaces/gwfdn_interface.cpp
     )
 
 # Pack hydrostatic module files
