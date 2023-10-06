0e06bf19411ace47259b29967c4fd8f7a5d8163d
sfruano <sergio.fernandezruano@unican.es>
2023-10-06 13:30:09 +0200

    Changed function to loop over the line an to detect characters associated with an empty line along all the characters in the line. This will prevent to skip empty lines when they have multiple tab characters or white spaces.

--- src/tools.cpp
+++ src/tools.cpp
@@ -70,17 +70,22 @@ bool check_num_cmd_args(int argc, int req_argc)
 
 bool is_empty_line( std::string line )
 {
-    bool is_empty = (
-                        line.empty( )
-                        ||
-                        line.compare( " " ) == 0
-                        ||
-                        line.compare( "  " ) == 0
-                        ||
-                        line.compare( "\t" ) == 0
-                        ||
-                        line.compare( "\n" ) == 0
-                    );
+    bool is_empty = true;
+    for (char& c : line )
+    {
+        if (
+                !(
+                    c == ' '
+                    ||
+                    c == '\t'
+                    ||
+                    c == '\n'
+                )
+            )
+        {
+            is_empty = false;
+        }
+    }
 
     return is_empty;
 }
