363ad714db2a5593dc067c8ba0c24524f4586769
sfruano <sergio.fernandezruano@unican.es>
2023-11-15 11:27:35 +0100

    Added wave amplitude as input variable. Usually, it will be set to one but it is addeed as input variable to change the value for special situations.

--- src/inout/input.hpp
+++ src/inout/input.hpp
@@ -55,6 +55,7 @@ public:
     cusfloat                    press_rel_err       = 0.0;
     cusfloat                    water_density       = 0.0;
     cusfloat                    water_depth         = 0.0;
+    cusfloat                    wave_amplitude      = 0.0;
     cusfloat                    wl_det_prec         = 0.0;
 
     // Define class constructors and destructors
