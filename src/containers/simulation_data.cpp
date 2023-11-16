c1a8b61abdffcfc4c4c8583fef33bc8feb2a2040
sfruano <sergio.fernandezruano@unican.es>
2023-11-16 18:39:48 +0100

    Added container to storage simulation related data. This will help to pack all the info and to keep the code as clean as possible.

---
+++ src/containers/simulation_data.cpp
@@ -0,0 +1,66 @@
+
+// Include local modules
+#include "../math/math_tools.hpp"
+#include "simulation_data.hpp"
+
+
+SimulationData::SimulationData(
+                                    int         bodies_np,
+                                    int         dofs_np,
+                                    int         heads_np,
+                                    int         rows_local_np,
+                                    int         cols_local_np,
+                                    MpiConfig*  mpi_config_in
+                                )
+{
+    // Storage input arguments into class attributes
+    this->hydmech_np        = pow2s( dofs_np * bodies_np );
+    this->_mpi_config       = mpi_config_in;  
+    this->wave_exc_np       = heads_np * bodies_np * dofs_np;
+
+    // Allocate space variables used in all the processes
+    this->added_mass        = generate_empty_vector<cusfloat>( this->hydmech_np );
+    this->damping_rad       = generate_empty_vector<cusfloat>( this->hydmech_np );
+    this->froude_krylov     = generate_empty_vector<cuscomplex>( this->wave_exc_np );
+    this->raos              = generate_empty_vector<cuscomplex>( this->wave_exc_np );
+    this->intensities       = generate_empty_vector<cuscomplex>( ( dofs_np + heads_np ) * rows_local_np );
+    this->wave_diffrac      = generate_empty_vector<cuscomplex>( this->wave_exc_np );
+
+    // Allocate space for variables used only on root processor
+    if ( this->_mpi_config->is_root( ) )
+    {
+        this->added_mass_p0         = generate_empty_vector<cusfloat>( hydmech_np );
+        this->damping_rad_p0        = generate_empty_vector<cusfloat>( hydmech_np );
+        this->froude_krylov_p0      = generate_empty_vector<cuscomplex>( wave_exc_np );
+        this->hydrostiff_p0         = generate_empty_vector<cusfloat>( hydmech_np );
+        this->structural_mass_p0    = generate_empty_vector<cusfloat>( hydmech_np );
+        this->wave_diffrac_p0       = generate_empty_vector<cuscomplex>( wave_exc_np );
+        this->wave_exc_p0           = generate_empty_vector<cuscomplex>( wave_exc_np );
+    }
+}
+
+
+SimulationData::~SimulationData(
+                                    void
+                                )
+{
+    mkl_free( this->added_mass );
+    mkl_free( this->damping_rad );
+    mkl_free( this->froude_krylov );
+    mkl_free( this->raos );
+    mkl_free( this->intensities );
+    mkl_free( this->sysmat );
+    mkl_free( this->sysmat_steady );
+    mkl_free( this->wave_diffrac );
+
+    if ( this->_mpi_config->is_root( ) )
+    {
+        mkl_free( this->added_mass_p0 );
+        mkl_free( this->damping_rad_p0 );
+        mkl_free( this->froude_krylov_p0 );
+        mkl_free( this->hydrostiff_p0 );
+        mkl_free( this->structural_mass_p0 );
+        mkl_free( this->wave_diffrac_p0 );
+        mkl_free( this->wave_exc_p0 );
+    }
+}
---
+++ src/containers/simulation_data.hpp
@@ -0,0 +1,53 @@
+
+#ifndef __simulation_data_hpp
+#define __simulation_data_hpp
+
+// Include local modules
+#include "../config.hpp"
+#include "mpi_config.hpp"
+
+
+struct SimulationData
+{
+private:
+    // Declare private class attributes
+    MpiConfig*  _mpi_config         = nullptr;
+
+public:
+    // Declare class attributes
+    cusfloat*   added_mass          = nullptr;
+    cusfloat*   added_mass_p0       = nullptr;
+    cusfloat*   damping_rad         = nullptr;
+    cusfloat*   damping_rad_p0      = nullptr;
+    cuscomplex* froude_krylov       = nullptr;
+    cuscomplex* froude_krylov_p0    = nullptr;
+    int         hydmech_np          = 0;
+    cusfloat*   hydrostiff_p0       = nullptr;
+    cuscomplex* intensities         = nullptr;
+    cuscomplex* raos                = nullptr;
+    cusfloat*   structural_mass_p0  = nullptr;
+    cuscomplex* sysmat              = nullptr;
+    cuscomplex* sysmat_steady       = nullptr;
+    cuscomplex* wave_diffrac        = nullptr;
+    cuscomplex* wave_diffrac_p0     = nullptr;
+    int         wave_exc_np         = 0;
+    cuscomplex* wave_exc_p0         = nullptr;
+
+    // Declare class constructors and destructor
+    SimulationData( ) = default;
+
+    SimulationData(
+                        int         bodies_np,
+                        int         dofs_np,
+                        int         heads_np,
+                        int         rows_local_np,
+                        int         cols_local_np,
+                        MpiConfig*  mpi_config_in
+                    );
+
+    ~SimulationData( 
+                        void
+                    );
+};
+
+#endif
