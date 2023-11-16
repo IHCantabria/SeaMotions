bcc1981322a8d441363a2a1167bc708b27c86623
sfruano <sergio.fernandezruano@unican.es>
2023-11-16 18:46:28 +0100

    Implemented functions to calculate the wave elevation and the relative wave elevation w.r.t the WL points.

--- src/solvers/freq_domain/wave_elevation.cpp
+++ src/solvers/freq_domain/wave_elevation.cpp
@@ -3,35 +3,91 @@
 #include "../../config.hpp"
 #include "../../inout/input.hpp"
 #include "../../containers/mpi_config.hpp"
+#include "../../math/euler_transforms.hpp"
 #include "../../mesh/mesh_group.hpp"
+#include "../../solvers/freq_domain/potential.hpp"
 #include "wave_elevation.hpp"
 
 
 void    calculate_relative_wave_elevation_lin(
                                                 Input*          input,
                                                 MpiConfig*      mpi_config,
-                                                MeshGroup*      mesh_gp,
-                                                cuscomplex*     sources,
-                                                cuscomplex*     pot_steady,
+                                                cusfloat*       cog_to_fp,
+                                                int*            fp_cnp,
+                                                int             fp_nb,
+                                                cuscomplex*     potpanel_total,
+                                                cusfloat        ang_freq,
                                                 cuscomplex*     raos,
-                                                cusfloat*       field_points,
-                                                int             field_points_np
+                                                cuscomplex*     rel_wave_elevation
                                             )
 {
-    /****************************************************/
-    /************ Calculate wave elevation **************/
-    /****************************************************/
-
-    // Define parallel chunks
-    int start_col   = 0;
-    int end_col     = 0;
-    mpi_config->get_1d_bounds( 
-                                    mesh_gp->panels_wl_tnp, 
-                                    start_col, 
-                                    end_col
+    // Calculate wave elevation
+    calculate_wave_elevation_lin(
+                                    potpanel_total,
+                                    fp_cnp[fp_nb],
+                                    ang_freq,
+                                    input->grav_acc,
+                                    rel_wave_elevation
                                 );
-    int ipm_cols_np = start_col - end_col;
 
-    
+    // Calculate relative wave elevation
+    cusfloat    cog_to_point[3] = { 0.0, 0.0, 0.0 };
+    int         fp_np           = fp_cnp[fp_nb];
+    int         we_index        = 0;
+    cusfloat    point_disp[3]   = { 0.0, 0.0, 0.0 };
+    cusfloat    rao_trans[3]    = { 0.0, 0.0, 0.0 };
+    cusfloat    rao_rot[3]      = { 0.0, 0.0, 0.0 };
+    int         rao_index       = 0;
+
+    for ( int i=0; i<input->heads_np; i++ )
+    {
+        for ( int j=0; j<fp_nb; j++ )
+        {
+            rao_index       =  (
+                                    i * ( input->dofs_np * fp_nb )
+                                    +
+                                    j * input->dofs_np
+                                );
+            rao_trans[0]    = std::abs( raos[rao_index] );
+            rao_trans[1]    = std::abs( raos[rao_index+1] );
+            rao_trans[2]    = std::abs( raos[rao_index+2] );
+
+            rao_rot[0]      = std::abs( raos[rao_index+3] );
+            rao_rot[1]      = std::abs( raos[rao_index+4] );
+            rao_rot[2]      = std::abs( raos[rao_index+5] );
+
+            for ( int k=fp_cnp[j]; k<fp_cnp[j+1]; k++ )
+            {
+                // Calculate movement of the kth point at the WL
+                euler_local_to_global_disp( 
+                                                rao_trans,
+                                                rao_rot,
+                                                &(cog_to_fp[3*k]),
+                                                point_disp
+                                            );
+
+                // Calculate relative water height by substracting the 
+                // vertical motion at the required point to the wave elevation
+                we_index                        = i * fp_np + k;
+                rel_wave_elevation[we_index]    -= point_disp[2];
+            }
+        }
+    }
+ 
+}
+
+
+void    calculate_wave_elevation_lin(
+                                        cuscomplex*     pot_total,
+                                        int             pot_total_np,
+                                        cusfloat        ang_freq,
+                                        cusfloat        grav_acc,
+                                        cuscomplex*     wave_elevation
+                                    )
+{
+    for ( int i=0; i<pot_total_np; i++ )
+    {
+        wave_elevation[i] = pot_total[i].imag( ) * ang_freq / grav_acc;
+    }
 
 }
--- src/solvers/freq_domain/wave_elevation.hpp
+++ src/solvers/freq_domain/wave_elevation.hpp
@@ -2,6 +2,25 @@
 #ifndef __fd_wave_elevation_hpp
 #define __fd_wave_elevation_hpp
 
+void    calculate_relative_wave_elevation_lin(
+                                                Input*          input,
+                                                MpiConfig*      mpi_config,
+                                                cusfloat*       cog_to_fp,
+                                                int*            fp_cnp,
+                                                int             fp_nb,
+                                                cuscomplex*     potpanel_total,
+                                                cusfloat        ang_freq,
+                                                cuscomplex*     raos,
+                                                cuscomplex*     rel_wave_elevation
+                                            );
 
+
+void    calculate_wave_elevation_lin(
+                                                cuscomplex*     pot_total,
+                                                int             pot_total_np,
+                                                cusfloat        ang_freq,
+                                                cusfloat        grav_acc,
+                                                cuscomplex*     wave_elevation
+                                    );
 
 #endif
