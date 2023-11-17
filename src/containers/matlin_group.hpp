436a7453e680be41a261964115a56a7c294a681f
sfruano <sergio.fernandezruano@unican.es>
2023-11-17 11:10:41 +0100

    Changed class behaviour implementation. Assumed 3 dimensions and added new input argument to control the number of field values that can be storaged in the class.

--- src/containers/matlin_group.hpp
+++ src/containers/matlin_group.hpp
@@ -9,17 +9,21 @@
 template <typename T>
 struct MatLinGroup
 {
+private:
+    // Define class private attributes
+    int         _dims_np            = 3;
+
 public:
     // Define class public attributes
     cusfloat*   cog_to_field_points = nullptr;
     int         end_col             = 0;
     int         end_row             = 0;
     cusfloat*   field_points        = nullptr;
-    int         field_points_nb     = 0;
-    int         field_points_nf     = 0;
-    int         field_points_np     = 0;
     int*        field_points_cnp    = nullptr;
+    int         field_points_nb     = 0;
+    int         field_points_np     = 0;
     T*          field_values        = nullptr;
+    int         fields_np           = 0;
     int         start_col           = 0;
     int         start_row           = 0;
     T*          sysmat              = nullptr;
@@ -31,8 +35,8 @@ public:
     MatLinGroup(
                     int sysmat_nrows_in,
                     int sysmat_ncols_in,
-                    int field_points_nf_in,
                     int field_points_nb_in,
+                    int fields_np_in,
                     int start_row_in,
                     int end_row_in,
                     int start_col_in,
@@ -52,8 +56,8 @@ template <typename T>
 MatLinGroup<T>::MatLinGroup(
                                 int sysmat_nrows_in,
                                 int sysmat_ncols_in,
-                                int field_points_nf_in,
                                 int field_points_nb_in,
+                                int fields_np_in,
                                 int start_row_in,
                                 int end_row_in,
                                 int start_col_in,
@@ -64,8 +68,8 @@ MatLinGroup<T>::MatLinGroup(
     this->end_col           = end_col_in;
     this->end_row           = end_row_in;
     this->field_points_nb   = field_points_nb_in;
-    this->field_points_nf   = field_points_nf_in;
     this->field_points_np   = sysmat_nrows_in;
+    this->fields_np         = fields_np_in;
     this->start_col         = start_col_in;
     this->start_row         = start_row_in;
     this->sysmat_nrows      = sysmat_nrows_in;
@@ -76,10 +80,10 @@ MatLinGroup<T>::MatLinGroup(
     this->sysmat_steady     = generate_empty_vector<T>( this->sysmat_nrows * this->sysmat_ncols );
 
     // Allocate space for the field points and the field values
-    this->cog_to_field_points   = generate_empty_vector<T>( this->field_points_nf * this->sysmat_nrows );
-    this->field_points          = generate_empty_vector<T>( this->field_points_nf * this->sysmat_nrows );
+    this->cog_to_field_points   = generate_empty_vector<T>( this->_dims_np * this->sysmat_nrows );
+    this->field_points          = generate_empty_vector<T>( this->_dims_np * this->sysmat_nrows );
     this->field_points_cnp      = generate_empty_vector<int>( this->field_points_nb+1 );
-    this->field_values          = generate_empty_vector<T>( this->sysmat_nrows );
+    this->field_values          = generate_empty_vector<T>( this->fields_np * this->sysmat_nrows );
 }
 
 
