65e1d6eb9e9c52979c9eab49c155684ea247ebf5
sfruano <sergio.fernandezruano@unican.es>
2024-01-12 10:14:54 +0100

    Added new class to storage second order wave dispersion values.

---
+++ src/waves/wave_dispersion_so.cpp
@@ -0,0 +1,110 @@
+
+// Include local modules
+#include "wave_dispersion_so.hpp"
+
+#include "waves_common.hpp"
+
+
+void    WaveDispersionSO::set_new_data(
+                                            cusfloat w0_in,
+                                            cusfloat w1_in,
+                                            cusfloat head_0_in,
+                                            cusfloat head_1_in
+                                        )
+{
+    this->_update_status(
+                            w0_in,
+                            w1_in,
+                            head_0_in,
+                            head_1_in
+                        );
+}
+
+
+void    WaveDispersionSO::_update_status(
+                                            cusfloat    w0_in,
+                                            cusfloat    w1_in,
+                                            cusfloat    head_0_in,
+                                            cusfloat    head_1_in
+                                        )
+{
+    // Storage incoming attributes
+    this->head_0        = head_0_in;
+    this->head_1        = head_1_in;
+    this->w0            = w0_in;
+    this->w1            = w1_in;
+
+    // Calculate wave numbers
+    this->k0            = w2k( this->w0, this->water_depth, this->grav_acc );
+    this->k1            = w2k( this->w1, this->water_depth, this->grav_acc );
+
+    this->k0_deep_water = pow2s( this->w0 ) / this->grav_acc;
+    this->k1_deep_water = pow2s( this->w1 ) / this->grav_acc;
+
+    this->k0_vec[0]     = this->k0 * std::cos( this->head_0 );
+    this->k0_vec[1]     = this->k0 * std::sin( this->head_0 );
+
+    this->k1_vec[0]     = this->k1 * std::cos( this->head_1 );
+    this->k1_vec[1]     = this->k1 * std::sin( this->head_1 );
+
+    // Calculate sum and difference terms
+    this->w_diff        = this->w0 - this->w1;
+    this->w_sum         = this->w0 + this->w1;
+
+    this->k_diff_mod    = std::sqrt(
+                                        pow2s( this->k0 )
+                                        +
+                                        pow2s( this->k1 )
+                                        -
+                                        2 * this->k0 * this->k1 * std::cos( this->head_0 - this->head_1 )
+                                    );
+    
+    this->k_sum_mod     = std::sqrt(
+                                        pow2s( this->k0 )
+                                        +
+                                        pow2s( this->k1 )
+                                        +
+                                        2 * this->k0 * this->k1 * std::cos( this->head_0 - this->head_1 )
+                                    );
+
+    sv_sub( 
+                2,
+                this->k0_vec, 
+                this->k1_vec,
+                this->k_diff_vec
+            );
+    sv_add( 
+                2, 
+                this->k0_vec,
+                this->k1_vec,
+                this->k_sum_vec
+            );
+
+    // Calculate equivalent angular frequency for the wave lenghts difference
+    this->w_k_diff  = k2w( this->k_diff_mod, this->water_depth, this->grav_acc );
+    this->w_k_sum   = k2w( this->k_sum_mod, this->water_depth, this->grav_acc );
+
+}
+
+
+WaveDispersionSO::WaveDispersionSO(
+                                        cusfloat    w0_in,
+                                        cusfloat    w1_in,
+                                        cusfloat    head_0_in,
+                                        cusfloat    head_1_in,
+                                        cusfloat    water_depth_in,
+                                        cusfloat    grav_acc_in
+                                    )
+{
+    // Storage general usage input arguments
+    this->grav_acc      = grav_acc_in;
+    this->water_depth   = water_depth_in;
+
+    // Update object status
+    this->_update_status( 
+                            w0_in,
+                            w1_in,
+                            head_0_in,
+                            head_1_in
+                        );
+}
---
+++ src/waves/wave_dispersion_so.hpp
@@ -0,0 +1,65 @@
+
+#ifndef __wave_dispersion_so_hpp
+#define __wave_dispersion_so_hpp
+
+// Include local modules
+#include "../config.hpp"
+
+
+struct WaveDispersionSO
+{
+private:
+    // Define private class methods
+    void    _update_status(
+                                cusfloat    w0_in,
+                                cusfloat    w1_in,
+                                cusfloat    head_0_in,
+                                cusfloat    head_1_in
+                            );
+
+public:
+    // Define public class attributes
+    cusfloat    grav_acc        = 0.0;
+    cusfloat    head_0          = 0.0;
+    cusfloat    head_1          = 0.0;
+    cusfloat    k0              = 0.0;
+    cusfloat    k0_deep_water   = 0.0;
+    cusfloat    k0_vec[2]       = { 0.0, 0.0 };
+    cusfloat    k1              = 0.0;
+    cusfloat    k1_deep_water   = 0.0;
+    cusfloat    k1_vec[2]       = { 0.0, 0.0 };
+    cusfloat    k_diff_mod      = 0.0;
+    cusfloat    k_diff_vec[2]   = { 0.0, 0.0 };
+    cusfloat    k_sum_mod       = 0.0;
+    cusfloat    k_sum_vec[2]    = { 0.0, 0.0 };
+    cusfloat    w0              = 0.0;
+    cusfloat    w1              = 0.0;
+    cusfloat    water_depth     = 0.0;
+    cusfloat    w_diff          = 0.0;
+    cusfloat    w_k_diff        = 0.0;
+    cusfloat    w_k_sum         = 0.0;
+    cusfloat    w_sum           = 0.0;
+
+    // Define class constructors and destructor
+    WaveDispersionSO( ) = default;
+
+    WaveDispersionSO(
+                        cusfloat    w0_in,
+                        cusfloat    w1_in,
+                        cusfloat    head_0_in,
+                        cusfloat    head_1_in,
+                        cusfloat    water_depth,
+                        cusfloat    grav_acc
+                    );
+
+    // Define class methods
+    void    set_new_data(
+                            cusfloat w0_in,
+                            cusfloat w1_in,
+                            cusfloat head_0_in,
+                            cusfloat head_1_in
+                        );
+
+};
+
+#endif
