02f0e5871158d6743e6abfc6abb0ecc058d1e497
sfruano <sergio.fernandezruano@unican.es>
2025-06-28 12:00:28 +0200

    Added chebyshev traits for all the database integrals

--- src/green/chebyshev_traits.hpp
+++ src/green/chebyshev_traits.hpp
@@ -4,39 +4,47 @@
 
 // Include local modules
 #include "chebyshev_traits_macros.hpp"
-#include "./inf_depth_coeffs/R44.hpp"
-#include "./inf_depth_coeffs/R44_dX.hpp"
+#include "./fin_depth_coeffs/L1.hpp"
+#include "./fin_depth_coeffs/L1_dA.hpp"
+#include "./fin_depth_coeffs/L1_dB.hpp"
+#include "./fin_depth_coeffs/L2.hpp"
+#include "./fin_depth_coeffs/L3.hpp"
+#include "./fin_depth_coeffs/L3_dA.hpp"
+#include "./fin_depth_coeffs/L3_dB.hpp"
+#include "./fin_depth_coeffs/M1.hpp"
+#include "./fin_depth_coeffs/M1_dA.hpp"
+#include "./fin_depth_coeffs/M1_dB.hpp"
+#include "./fin_depth_coeffs/M2.hpp"
+#include "./fin_depth_coeffs/M3.hpp"
+#include "./fin_depth_coeffs/M3_dA.hpp"
+#include "./fin_depth_coeffs/M3_dB.hpp"
+#include "./inf_depth_coeffs/R11.hpp"
+#include "./inf_depth_coeffs/R11_dX.hpp"
 
 
 // Generic trait declaration
 template<typename NS>
 struct ChebyshevTraits;
 
-// Instantiate traits through the corresponding macro
-CHEBYSHEV_2D_TRAITS( R44C );
-CHEBYSHEV_2D_TRAITS( R44_dXC );
+// Expand macros for infinite water depth traits
+CHEBYSHEV_2D_TRAITS( R11C );
+CHEBYSHEV_2D_TRAITS( R11_dXC );
 
-// template<>                                                                                 
-// struct ChebyshevTraits<R44C>                                                                 
-// {                                                                                          
-//     static constexpr            int             max_ref_level       = R44C::max_ref_level;   
-//     static constexpr            int             intervals_np        = R44C::intervals_np;    
-//     static constexpr            int             max_cheby_order     = R44C::max_cheby_order; 
-//     static constexpr const      std::size_t*    blocks_start        = R44C::blocks_start;    
-//     static constexpr const      std::size_t*    blocks_coeffs_np    = R44C::blocks_coeffs_np;
-//     static constexpr            bool            fcn_log_scale       = R44C::fcn_log_scale;   
-//     static constexpr            bool            x_log_scale         = R44C::x_log_scale;     
-//     static constexpr            cusfloat        x_max               = R44C::x_max;           
-//     static constexpr            cusfloat        x_min               = R44C::x_min;           
-//     static constexpr            cusfloat        dx                  = R44C::dx;              
-//     static constexpr            bool            y_log_scale         = R44C::y_log_scale;     
-//     static constexpr            cusfloat        y_max               = R44C::y_max;           
-//     static constexpr            cusfloat        y_min               = R44C::y_min;           
-//     static constexpr            cusfloat        dy                  = R44C::dy;              
-//     static constexpr            std::size_t     num_c               = R44C::num_c;        
-//     static constexpr const      cusfloat*       coeffs              = R44C::c;            
-//     static constexpr const      std::size_t*    ncx                 = R44C::ncx; 
-//     static constexpr const      std::size_t*    ncy                 = R44C::ncy;          
-// };    
+// Expand macros for finit water depth traits
+CHEBYSHEV_3DF_TRAITS( L1C )
+CHEBYSHEV_3DF_TRAITS( L1_dAC )
+CHEBYSHEV_3DF_TRAITS( L1_dBC )
+CHEBYSHEV_1DF_TRAITS( L2C )
+CHEBYSHEV_3DF_TRAITS( L3C )
+CHEBYSHEV_3DF_TRAITS( L3_dAC )
+CHEBYSHEV_3DF_TRAITS( L3_dBC )
+
+CHEBYSHEV_3DF_TRAITS( M1C )
+CHEBYSHEV_3DF_TRAITS( M1_dAC )
+CHEBYSHEV_3DF_TRAITS( M1_dBC )
+CHEBYSHEV_1DF_TRAITS( M2C )
+CHEBYSHEV_3DF_TRAITS( M3C )
+CHEBYSHEV_3DF_TRAITS( M3_dAC )
+CHEBYSHEV_3DF_TRAITS( M3_dBC )
 
 #endif
