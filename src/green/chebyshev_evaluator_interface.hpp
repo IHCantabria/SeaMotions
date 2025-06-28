c10effa2bfdf6274713f7342fa251eb79bdcafeb
sfruano <sergio.fernandezruano@unican.es>
2025-06-28 12:02:51 +0200

    Added chebyshev interpolation traits interfaces for the database integrals.

--- src/green/chebyshev_evaluator_interface.hpp
+++ src/green/chebyshev_evaluator_interface.hpp
@@ -9,18 +9,60 @@
 /*************************************/
 /******* Alias Scalar Classes ********/
 /*************************************/
-using R44CE         = ChebyshevEvaluatorBase<ChebyshevTraits<R44C>>;
-using R44_dXCE      = ChebyshevEvaluatorBase<ChebyshevTraits<R44_dXC>>;
+using R11CE         = ChebyshevEvaluatorBase<ChebyshevTraits<R11C>>;
+using R11_dXCE      = ChebyshevEvaluatorBase<ChebyshevTraits<R11_dXC>>;
 
 
 /*************************************/
 /******* Alias Vector Classes ********/
 /*************************************/
 template<std::size_t N>
-using R44CEV        = ChebyshevEvaluatorBaseVector<ChebyshevTraits<R44C>, N>;
+using L1CEV         = ChebyshevEvaluatorBaseVector<ChebyshevTraits<L1C>, N>;
 
 template<std::size_t N>
-using R44_dXCEV     = ChebyshevEvaluatorBaseVector<ChebyshevTraits<R44_dXC>, N>;
+using L1_dACEV      = ChebyshevEvaluatorBaseVector<ChebyshevTraits<L1_dAC>, N>;
+
+template<std::size_t N>
+using L1_dBCEV      = ChebyshevEvaluatorBaseVector<ChebyshevTraits<L1_dBC>, N>;
+
+template<std::size_t N>
+using L2CEV         = ChebyshevEvaluatorBaseVector<ChebyshevTraits<L2C>, N>;
+
+template<std::size_t N>
+using L3CEV         = ChebyshevEvaluatorBaseVector<ChebyshevTraits<L3C>, N>;
+
+template<std::size_t N>
+using L3_dACEV      = ChebyshevEvaluatorBaseVector<ChebyshevTraits<L3_dAC>, N>;
+
+template<std::size_t N>
+using L3_dBCEV      = ChebyshevEvaluatorBaseVector<ChebyshevTraits<L3_dBC>, N>;
+
+template<std::size_t N>
+using M1CEV         = ChebyshevEvaluatorBaseVector<ChebyshevTraits<M1C>, N>;
+
+template<std::size_t N>
+using M1_dACEV      = ChebyshevEvaluatorBaseVector<ChebyshevTraits<M1_dAC>, N>;
+
+template<std::size_t N>
+using M1_dBCEV      = ChebyshevEvaluatorBaseVector<ChebyshevTraits<M1_dBC>, N>;
+
+template<std::size_t N>
+using M2CEV         = ChebyshevEvaluatorBaseVector<ChebyshevTraits<M2C>, N>;
+
+template<std::size_t N>
+using M3CEV         = ChebyshevEvaluatorBaseVector<ChebyshevTraits<M3C>, N>;
+
+template<std::size_t N>
+using M3_dACEV      = ChebyshevEvaluatorBaseVector<ChebyshevTraits<M3_dAC>, N>;
+
+template<std::size_t N>
+using M3_dBCEV      = ChebyshevEvaluatorBaseVector<ChebyshevTraits<M3_dBC>, N>;
+
+template<std::size_t N>
+using R11CEV        = ChebyshevEvaluatorBaseVector<ChebyshevTraits<R11C>, N>;
+
+template<std::size_t N>
+using R11_dXCEV     = ChebyshevEvaluatorBaseVector<ChebyshevTraits<R11_dXC>, N>;
 
 
 
