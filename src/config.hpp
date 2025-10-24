49e05e08f68d3719172a121ef50c2dde87847004
sfruano <sergio.fernandezruano@unican.es>
2025-10-24 13:12:49 +0200

    + Added new configuration variables to control static compilation and for the free surface threshold.

--- src/config.hpp
+++ src/config.hpp
@@ -62,12 +62,17 @@ constexpr int       DGDR_OFF                = 0;                // Flag used as 
 constexpr int       DGDZ_ON                 = 1;                // Flag used as template argument to SET the calculation of the derivative of the potential green function with respect to the vertical coordinate
 constexpr int       DGDZ_OFF                = 0;                // Flag used as template argument to NOT SET the calculation of the derivative of the potential green function with respect to the vertical coordinate
 constexpr cusfloat  FIELD_POINT_LOCAL_TOL   = 1E-2;
-constexpr int       NUM_GP                  = 4;                // Number of Gauss Points used for numerical integration
+constexpr cusfloat  FS_SEL_THR              = 1e-3;             // Free surface selection threshold. All the points which z is below this threshold are considered to be in the free surface
+constexpr int       FSLID_ON                = 1;                // Flag used as template argument to REMOVE log singularity for free surface panels
+constexpr int       FSLID_OFF               = 0;                // Flag used as template argument to NOT REMOVE log singularity for free surface panels
+constexpr int       NUM_GP                  = 1;                // Number of Gauss Points used for numerical integration
 constexpr int       NUM_GP2                 = NUM_GP*NUM_GP;    // Squared number of gauss points ( just for convenience along the code )
 constexpr int       NUM_GP3                 = NUM_GP2*NUM_GP;   // Cubic number of gauss points ( just for convenience along the code )
 constexpr int       NUM_KN                  = 30;               // Maximum number of imaginary wave number roots used in the john series expansion
-constexpr int       STATIC_LOOP_ON          = 0;                // Flag used as template argument to set that the bounds of the internal loops of the calling function are known at compile time
-constexpr int       STATIC_LOOP_OFF         = 0;                // Flag used as template argument to set that the bounds of the internal loops of the calling function are not known at compile time
+constexpr int       PF_ON                   = 1;                // Flag used as template argument to SET the software to use the potential formulation to calculate the potential
+constexpr int       PF_OFF                  = 0;                // Flag used as template argument to NOT SET the software to use the potential formulation to calculate the potential
+constexpr int       STATIC_LOOP_ON          = 1;                // Flag used as template argument to SET that the bounds of the internal loops of the calling function are known at compile time
+constexpr int       STATIC_LOOP_OFF         = 0;                // Flag used as template argument to NOT SET that the bounds of the internal loops of the calling function are not known at compile time
 constexpr cusfloat  ZEROTH_EPS              = 1E-14;
 
 #endif
