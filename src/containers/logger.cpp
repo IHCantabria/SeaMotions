9447acaa258865af7db493c4146a0773c413dcb9
sfruano <sergio.fernandezruano@unican.es>
2025-11-22 19:15:23 +0100

    + Added new class to log task, info, warn and error messages. It implements algorithms to work with MpiTimer to measure task times.

---
+++ src/containers/logger.cpp
@@ -0,0 +1,126 @@
+
+// Include local modules
+#include "logger.hpp"
+
+
+const char* Logger::_color( Level level ) const 
+{
+    switch ( level ) 
+    {
+        case Level::INFO:    return TermColor::WHITE;
+        case Level::WARNING: return TermColor::YELLOW;
+        case Level::ERROR:   return TermColor::RED;
+        case Level::TASK:    return TermColor::GREEN;
+    }
+
+    return TermColor::WHITE;
+}
+
+
+void Logger::error( const std::string &msg )
+{ 
+    this->log( Level::ERROR, msg );
+}
+
+
+void Logger::info( const std::string &msg )
+{ 
+    this->log( Level::INFO, msg );
+}
+
+
+void Logger::log( Level level, const std::string& msg ) 
+{
+    if ( this->_is_root )
+    {
+        std::cout   << this->_color( level ) << this->_prefix( level )
+                    << msg << TermColor::RESET << std::endl << std::flush;
+    }
+}
+
+
+Logger::Logger( bool is_root )
+{
+    // Storage input arguments
+    this->_is_root = is_root;
+}
+
+
+Logger::Logger( bool is_root, int time_colum )
+{
+    // Storage input arguments
+    this->_is_root      = is_root;
+    this->_time_column  = time_colum;
+}
+
+
+std::string Logger::_prefix( Level level ) const 
+{
+    switch ( level ) 
+    {
+        case Level::INFO:    return "[INFO]: ";
+        case Level::WARNING: return "[WARNING]: ";
+        case Level::ERROR:   return "[ERROR]: ";
+        case Level::TASK:    return "[TASK]: ";
+    }
+
+    return "";
+}
+
+
+void Logger::set_time_column( int col ) 
+{ 
+    this->_time_column = col; 
+}
+
+
+void Logger::task( const std::string &msg ) 
+{
+    if ( this->_is_root )
+    {
+        this->_last_msg = msg;
+        std::cout << TermColor::GREEN << this->_prefix( Level::TASK );
+        std::cout << msg << TermColor::RESET << std::flush;
+    }
+}
+
+
+void Logger::task_time( const std::string& msg, MpiTimer& timer )
+{
+    // Get new message as last message to append the time
+    this->_last_msg = msg;
+
+    // Task time
+    this->task_time( timer );
+}
+
+
+void Logger::task_time( MpiTimer& timer )
+{
+    // Get time elapsed. Take care with the timer as 
+    // it request syncrhonization of all processes,
+    // it couldn't be only in the root process call.
+    std::ostringstream time_out;
+    time_out << " - Elapsed Time: " << timer;
+    std::string time_str = time_out.str( );
+
+    // Reject
+    if ( this->_is_root )
+    {
+        // Alignment padding
+        int padding = this->_time_column - static_cast<int>( this->_last_msg.size( ) );
+        if ( padding < 1 ) padding = 1;
+    
+        std::cout << std::string( padding, ' ' );
+        std::cout << TermColor::GREEN << " --> Done!" << TermColor::RESET;
+        std::cout << TermColor::CYAN << time_str << TermColor::RESET;
+        std::cout << "\n" << std::flush;
+    }
+
+}
+
+
+void Logger::warn( const std::string &msg )
+{ 
+    this->log( Level::WARNING, msg );
+}
---
+++ src/containers/logger.hpp
@@ -0,0 +1,99 @@
+
+#pragma once
+
+// Include general usage libraries
+#include <iostream>
+#include <iomanip>
+#include <mutex>
+#include <sstream>
+#include <string>
+
+// Include local module
+#include "mpi_timer.hpp"
+
+
+
+/*******************************************************************************/
+/***************************** Module Macros ***********************************/
+/*******************************************************************************/
+#define LOG_TASK( msg )                         \
+    Logger logger( mpi_config->is_root( ) );    \
+    logger.task( msg );                         \
+
+
+#define LOG_TASK_SS( msg )                      \
+    std::stringstream ss;                       \
+    ss << msg;                                  \
+    Logger logger( mpi_config->is_root( ) );    \
+    logger.task( ss.str( ) );                   \
+
+
+#define LOG_TASK_TIME( timer )                  \
+logger.task_time( timer );                      \
+
+
+/*******************************************************************************/
+/************************** ANSI Terminal Colors *******************************/
+/*******************************************************************************/
+namespace TermColor {
+                        static constexpr const char* RESET   = "\033[0m";
+                        static constexpr const char* RED     = "\033[31m";
+                        static constexpr const char* YELLOW  = "\033[33m";
+                        static constexpr const char* GREEN   = "\033[32m";
+                        static constexpr const char* CYAN    = "\033[36m";
+                        static constexpr const char* WHITE   = "\033[37m";
+                    }
+
+
+/*******************************************************************************/
+/************************ Define Enum Class Level ******************************/
+/*******************************************************************************/
+enum class Level { INFO, WARNING, ERROR, TASK };
+
+
+/*******************************************************************************/
+/****************************** Logger Class ***********************************/
+/*******************************************************************************/
+class Logger
+{
+private:
+    /* Declare private attributes */
+    bool                _is_root     = true;
+    std::string         _last_msg;
+    int                 _time_column = 40;
+    std::mutex          _mutex;
+
+    /* Declare private class methods */
+    const char*     _color( Level level ) const;
+
+    std::string     _prefix( Level lvl ) const;
+
+public:
+    /* Declare class constructors */
+    // Default constructor
+    Logger( ) = default;
+
+    // Constructor that allows to change root and time column
+    Logger( bool is_root );
+
+    // Constructor that allows to change root and time column
+    Logger( bool is_root, int time_column );
+
+    /* Declare class methods */
+    void log( Level level, const std::string& msg);
+    
+    void error( const std::string &msg );
+
+    void info( const std::string &msg );
+
+    void set_time_column( int col );
+    
+    void task( const std::string &msg );
+    
+    void task_time( const std::string& msg, MpiTimer& timer );
+
+    void task_time( MpiTimer& timer);
+
+    void warn( const std::string &msg );
+
+};
