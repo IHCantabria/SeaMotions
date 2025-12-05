98d584bdfac715215023b4b8cd280b7918a9c8ab
sfruano <sergio.fernandezruano@unican.es>
2025-12-05 13:45:27 +0100

    + Added GPLv3 header to math module files.

--- src/math/gauss.hpp
+++ src/math/gauss.hpp
@@ -1,6 +1,24 @@
 
-#ifndef __gauss_hpp
-#define __gauss_hpp
+/*
+ * Copyright (c) 2025 Sergio Fernández Ruano / IHCantabria
+ *
+ * This file is part of SeaMotions Software.
+ *
+ * SeaMotions is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SeaMotions is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
 
 // Include general usage scientific libraries
 #include "mkl.h"
@@ -736,5 +754,3 @@ public:
                                                                     };
 
 };
-
-#endif
--- src/math/random.hpp
+++ src/math/random.hpp
@@ -1,12 +1,26 @@
 
-#ifndef __random_hpp
-#define __random_hpp
+/*
+ * Copyright (c) 2025 Sergio Fernández Ruano / IHCantabria
+ *
+ * This file is part of SeaMotions Software.
+ *
+ * SeaMotions is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SeaMotions is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 
+#pragma once
 
 void        check_vsl_error( int num );
 int64_t     generate_random_seed( void );
 void        randn( double mean, double std, int seed, int np, double* data );
 void        randu( double a, double b, int seed, int np, double* data );
-
-
-#endif
