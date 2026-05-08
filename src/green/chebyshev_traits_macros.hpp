c0fad382bfaef947d3ba722c48f88bfc1f486bcc
sfruano <sergio.fernandezruano@unican.es>
2026-05-08 09:28:17 +0200

    + Added 2D fold chebyshev traits

--- src/green/chebyshev_traits_macros.hpp
+++ src/green/chebyshev_traits_macros.hpp
@@ -87,6 +87,38 @@ struct ChebyshevTraits<NS>                                                      
 };                                                                                                              \
 
 
+#define CHEBYSHEV_2DF_TRAITS( NS )                                                                                      \
+template<>                                                                                                              \
+struct ChebyshevTraits<NS>                                                                                              \
+{                                                                                                                       \
+            static constexpr            int             max_ref_level               = NS::max_ref_level;                \
+            static constexpr            int             intervals_np                = NS::intervals_np;                 \
+            static constexpr            int             max_cheby_order             = NS::max_cheby_order_f;            \
+    inline  static                      std::size_t*    blocks_start                = NS::blocks_start_f;               \
+    inline  static                      std::size_t*    blocks_coeffs_np            = NS::blocks_coeffs_np_f;           \
+    inline  static                      std::size_t*    blocks_max_cheby_order      = NS::blocks_max_cheby_order_f;     \
+            static constexpr            bool            fcn_log_scale               = NS::fcn_log_scale;                \
+            static constexpr            bool            x_log_scale                 = NS::x_log_scale;                  \
+            static constexpr            cusfloat        x_max_global                = NS::x_max_global;                 \
+            static constexpr            cusfloat        x_min_global                = NS::x_min_global;                 \
+            static constexpr            cusfloat        dx_min_region               = NS::dx_min_region;                \
+    inline  static                      cusfloat*       x_max_region                = NS::x_max_region_f;               \
+    inline  static                      cusfloat*       x_min_region                = NS::x_min_region_f;               \
+    inline  static                      cusfloat*       dx_region                   = NS::dx_region_f;                  \
+            static constexpr            bool            y_log_scale                 = NS::y_log_scale;                  \
+            static constexpr            cusfloat        y_max_global                = NS::y_max_global;                 \
+            static constexpr            cusfloat        y_min_global                = NS::y_min_global;                 \
+            static constexpr            cusfloat        dy_min_region               = NS::dy_min_region;                \
+    inline  static                      cusfloat*       y_max_region                = NS::y_max_region_f;               \
+    inline  static                      cusfloat*       y_min_region                = NS::y_min_region_f;               \
+    inline  static                      cusfloat*       dy_region                   = NS::dy_region_f;                  \
+            static constexpr            std::size_t     num_c                       = NS::num_cf;                       \
+    inline  static                      cusfloat*       coeffs                      = NS::cf;                           \
+    inline  static                      std::size_t*    ncx                         = NS::ncxf;                         \
+    inline  static                      std::size_t*    ncy                         = NS::ncyf;                         \
+};                                                                                                                      \
+
+
 #define CHEBYSHEV_3D_TRAITS( NS )                                                                               \
 template<>                                                                                                      \
 struct ChebyshevTraits<NS>                                                                                      \
